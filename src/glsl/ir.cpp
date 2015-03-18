36b58835c4a20208ba1681ce53e293a771df3a11
Samuel Iglesias Gonsalvez <siglesias@igalia.com>
2015-03-18 15:32:03 +0100

    glsl: add support for unsized arrays in shader storage blocks
    They only can be defined in the last position of the shader
    storage blocks.
    
    When an unsized array is used in different shaders, it might be
    converted in different sized arrays, avoid get a linker error
    in that case.
    
    v2:
    - Rework error condition and error messages (Timothy Arceri)
    
    Signed-off-by: Samuel Iglesias Gonsalvez <siglesias@igalia.com>

--- src/glsl/ir.cpp
+++ src/glsl/ir.cpp
@@ -1657,6 +1657,7 @@ ir_variable::ir_variable(const struct glsl_type *type, const char *name,
    this->data.image_coherent = false;
    this->data.image_volatile = false;
    this->data.image_restrict = false;
+   this->data.from_ssbo_unsized_array = false;
 
    if (type != NULL) {
       if (type->base_type == GLSL_TYPE_SAMPLER)
