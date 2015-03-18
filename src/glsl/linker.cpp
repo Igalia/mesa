78b5f82fd4605dcec2d4505ea3d49faf82e6b057
Samuel Iglesias Gonsalvez <siglesias@igalia.com>
2015-03-18 15:32:03 +0100

    glsl: add support for unsized arrays in shader storage blocks
    They only can be defined in the last position of the shader
    storage blocks.
    
    When an unsized array is used in different shaders, it might be
    converted in different sized arrays, avoid get a linker error
    in that case.
    
    Signed-off-by: Samuel Iglesias Gonsalvez <siglesias@igalia.com>

--- src/glsl/linker.cpp
+++ src/glsl/linker.cpp
@@ -769,12 +769,24 @@ cross_validate_globals(struct gl_shader_program *prog,
                       && existing->type->record_compare(var->type)) {
                      existing->type = var->type;
                   } else {
-                     linker_error(prog, "%s `%s' declared as type "
-                                  "`%s' and type `%s'\n",
-                                  mode_string(var),
-                                  var->name, var->type->name,
-                                  existing->type->name);
-                     return;
+                     /* If it is an unsized array in a Shader Storage Block,
+                      * two different shaders can access to different elements.
+                      * Because of that, they might be converted to different
+                      * sized arrays, then check that they are compatible but
+                      * forget about the array size.
+                      */
+                     if (!(var->data.mode == ir_var_buffer &&
+                           var->data.from_unsized_array &&
+                           existing->data.mode == ir_var_buffer &&
+                           existing->data.from_unsized_array &&
+                           var->type->gl_type == existing->type->gl_type)) {
+                        linker_error(prog, "%s `%s' declared as type "
+                                    "`%s' and type `%s'\n",
+                                    mode_string(var),
+                                    var->name, var->type->name,
+                                    existing->type->name);
+                        return;
+                     }
                   }
 	       }
 	    }
