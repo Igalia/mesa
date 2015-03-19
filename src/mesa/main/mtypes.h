ebe96d12cfce7e73c91fc63527c725e3bead032f
Iago Toral Quiroga <itoral@igalia.com>
2015-03-19 11:26:43 +0100

    mesa: Add ShaderStorageBufferOffsetAlignment to gl_context

--- src/mesa/main/mtypes.h
+++ src/mesa/main/mtypes.h
@@ -3367,6 +3367,7 @@ struct gl_constants
     * GL_ARB_shader_storage_buffer_object
     */
    GLuint MaxShaderStorageBufferBindings;
+   GLuint ShaderStorageBufferOffsetAlignment;
    /** @} */
 
    /**
