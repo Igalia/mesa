388867670d7cdafbd17a609ab00a08fe7e79b9e4
Iago Toral Quiroga <itoral@igalia.com>
2015-03-19 12:26:59 +0100

    mesa: Add MaxCombinedShaderStorageBlocks to gl_context

--- src/mesa/main/mtypes.h
+++ src/mesa/main/mtypes.h
@@ -3373,6 +3373,7 @@ struct gl_constants
    /** @{
     * GL_ARB_shader_storage_buffer_object
     */
+   GLuint MaxCombinedShaderStorageBlocks;
    GLuint MaxShaderStorageBufferBindings;
    GLuint ShaderStorageBufferOffsetAlignment;
    /** @} */
