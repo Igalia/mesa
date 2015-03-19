b31175e8fb4e3f143720356ea8071d62f25070b9
Iago Toral Quiroga <itoral@igalia.com>
2015-03-19 12:30:32 +0100

    mesa: Add MaxShaderStorageBlockSize to gl_context

--- src/mesa/main/mtypes.h
+++ src/mesa/main/mtypes.h
@@ -3368,6 +3368,7 @@ struct gl_constants
     */
    GLuint MaxCombinedShaderStorageBlocks;
    GLuint MaxShaderStorageBufferBindings;
+   GLuint MaxShaderStorageBlockSize;
    GLuint ShaderStorageBufferOffsetAlignment;
    /** @} */
 
