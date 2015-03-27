913cc2e367330a0b3b7793ca986b27c5114c3fdc
Iago Toral Quiroga <itoral@igalia.com>
2015-03-27 09:47:11 +0100

    mesa: Add new IR node ir_ssbo_store
    Shader storage buffer objects (SSBO) require special handling: when we
    detect writes to any channel of a shader buffer variable we need to
    emit the corresponding write to memory. We will later add a lowering pass
    that detects these writes and injects ir_ssbo_store nodes in the IR so
    drivers can  generate code for the memory writes.

--- src/mesa/drivers/dri/i965/brw_vec4_visitor.cpp
+++ src/mesa/drivers/dri/i965/brw_vec4_visitor.cpp
@@ -2973,6 +2973,12 @@ vec4_visitor::visit(ir_end_primitive *)
 }
 
 void
+vec4_visitor::visit(ir_ssbo_store *)
+{
+   unreachable("not implemented yet");
+}
+
+void
 vec4_visitor::emit_untyped_atomic(unsigned atomic_op, unsigned surf_index,
                                   dst_reg dst, src_reg offset,
                                   src_reg src0, src_reg src1)
