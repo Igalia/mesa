667e43d841f95934c8a289cc2bd33af8e6311b5c
Iago Toral Quiroga <itoral@igalia.com>
2015-04-07 15:58:20 +0200

    i965/vec4: Implement buffer writes with non-constant surface index
    This is necessary for SSBO instance arrays.

--- src/mesa/drivers/dri/i965/brw_vec4_generator.cpp
+++ src/mesa/drivers/dri/i965/brw_vec4_generator.cpp
@@ -1246,31 +1246,67 @@ vec4_generator::generate_buffer_write(vec4_instruction *inst,
       write_commit = true;
    }
 
-   /* FIXME:
-    * We probably need to support the case where index (block) is not a
-    * constant value
-    */
-
    /* Each of the 8 channel enables is considered for whether each
     * dword is written.
     */
-   brw_inst *send = brw_next_insn(p, BRW_OPCODE_SEND);
-   brw_set_dest(p, send, dst);
-   brw_set_src0(p, send, header);
-   if (brw->gen < 6)
-      brw_inst_set_cond_modifier(brw, send, inst->base_mrf);
-   brw_set_dp_write_message(p, send,
-                            index.dw1.ud,
-                            BRW_DATAPORT_OWORD_DUAL_BLOCK_1OWORD,
-                            msg_type,
-                            3, /* mlen */
-                            true, /* header present */
-                            false, /* not a render target write */
-                            write_commit, /* rlen */
-                            false, /* eot */
-                            write_commit);
+   if (index.file == BRW_IMMEDIATE_VALUE) {
+      brw_inst *send = brw_next_insn(p, BRW_OPCODE_SEND);
+      brw_set_dest(p, send, dst);
+      brw_set_src0(p, send, header);
+      if (brw->gen < 6)
+         brw_inst_set_cond_modifier(brw, send, inst->base_mrf);
+      brw_set_dp_write_message(p, send,
+                               index.dw1.ud,
+                               BRW_DATAPORT_OWORD_DUAL_BLOCK_1OWORD,
+                               msg_type,
+                               3, /* mlen */
+                               true, /* header present */
+                               false, /* not a render target write */
+                               write_commit, /* rlen */
+                               false, /* eot */
+                               write_commit);
 
-   brw_mark_surface_used(&prog_data->base, index.dw1.ud);
+      brw_mark_surface_used(&prog_data->base, index.dw1.ud);
+   } else {
+      struct brw_reg addr = vec1(retype(brw_address_reg(0), BRW_REGISTER_TYPE_UD));
+
+      brw_push_insn_state(p);
+      brw_set_default_mask_control(p, BRW_MASK_DISABLE);
+      brw_set_default_access_mode(p, BRW_ALIGN_1);
+
+      /* a0.0 = surf_index & 0xff */
+      brw_inst *insn_and = brw_next_insn(p, BRW_OPCODE_AND);
+      brw_inst_set_exec_size(p->brw, insn_and, BRW_EXECUTE_1);
+      brw_set_dest(p, insn_and, addr);
+      brw_set_src0(p, insn_and, vec1(retype(index, BRW_REGISTER_TYPE_UD)));
+      brw_set_src1(p, insn_and, brw_imm_ud(0x0ff));
+
+      /* a0.0 |= <descriptor> */
+      brw_inst *insn_or = brw_next_insn(p, BRW_OPCODE_OR);
+      brw_set_dp_write_message(p, insn_or,
+                               0,
+                               BRW_DATAPORT_OWORD_DUAL_BLOCK_1OWORD,
+                               msg_type,
+                               3, /* mlen */
+                               true, /* header present */
+                               false, /* not a render target write */
+                               write_commit, /* rlen */
+                               false, /* eot */
+                               write_commit);
+      brw_inst_set_exec_size(p->brw, insn_or, BRW_EXECUTE_1);
+      brw_inst_set_src1_reg_type(p->brw, insn_or, BRW_REGISTER_TYPE_UD);
+      brw_set_src0(p, insn_or, addr);
+      brw_set_dest(p, insn_or, addr);
+
+      brw_pop_insn_state(p);
+
+      /* dst = send(offset, a0.0) */
+      brw_inst *insn_send = brw_next_insn(p, BRW_OPCODE_SEND);
+      brw_set_dest(p, insn_send, dst);
+      brw_set_src0(p, insn_send, header);
+      brw_set_indirect_send_descriptor(p, insn_send,
+                                       GEN7_SFID_DATAPORT_DATA_CACHE, addr);
+   }
 }
 
 void
