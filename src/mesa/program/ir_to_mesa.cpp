a1b72d033ac7f76282cfa5453042e2f4e5bd3a15
Samuel Iglesias Gonsalvez <siglesias@igalia.com>
2015-04-13 16:17:07 +0200

    glsl: Add parser/compiler support for unsized array's length()
    It also creates unop and triop expressions to tell the driver to
    calculate the unsized array length.
    
    It is needed two expressions to do the calculation:
    
    * The unop expression saves the ir_rvalue* whose length should be
      calculated.
    * Afterwards, this unop is going to be processed by a lowering pass
    that will convert it to a triop that includes the block index,
    offset of the variable inside the shader storage block and the array
    stride. All of them are needed for length() calculation following
    GL_ARB_shader_storage_buffer spec:
    
       array.length() =
          max((buffer_object_size - offset_of_array) / stride_of_array, 0)
    
    Signed-off-by: Samuel Iglesias Gonsalvez <siglesias@igalia.com>

--- src/mesa/program/ir_to_mesa.cpp
+++ src/mesa/program/ir_to_mesa.cpp
@@ -1343,9 +1343,11 @@ ir_to_mesa_visitor::visit(ir_expression *ir)
    case ir_unop_dFdy_coarse:
    case ir_unop_dFdy_fine:
    case ir_binop_ssbo_load:
+   case ir_triop_ssbo_unsized_array_length:
       assert(!"not supported");
       break;
 
+   case ir_unop_ssbo_unsized_array_length:
    case ir_quadop_vector:
       /* This operation should have already been handled.
        */
