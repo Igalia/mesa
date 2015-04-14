ca6984233b950157ef64e60a3ed9eda793c102c5
Iago Toral Quiroga <itoral@igalia.com>
2015-04-14 17:15:54 +0200

    i965/fs: Add SHADER_OPCODE_UNALIGNED_BUFFER_LOAD opcode
    We will use this to implement non-constant offset read. This will
    allow us to handle both the cases of 16-byte aligned and unaligned
    non-constant offsets.

--- src/mesa/drivers/dri/i965/brw_fs_generator.cpp
+++ src/mesa/drivers/dri/i965/brw_fs_generator.cpp
@@ -1096,6 +1096,84 @@ fs_generator::generate_buffer_load(fs_inst *inst,
 }
 
 void
+fs_generator::generate_unaligned_buffer_load(fs_inst *inst,
+                                             struct brw_reg dst,
+                                             struct brw_reg index,
+                                             struct brw_reg offset)
+{
+   struct brw_reg header = brw_vec8_grf(0, 0);
+   gen6_resolve_implied_move(p, &header, inst->base_mrf);
+   struct brw_reg mrf =
+      retype(brw_message_reg(inst->base_mrf), BRW_REGISTER_TYPE_UD);
+
+   brw_push_insn_state(p);
+   brw_set_default_predicate_control(p, BRW_PREDICATE_NONE);
+   brw_set_default_compression_control(p, BRW_COMPRESSION_NONE);
+   brw_set_default_mask_control(p, BRW_MASK_DISABLE);
+
+   /* Header with Global Offset */
+   if (offset.file != BRW_IMMEDIATE_VALUE) {
+      offset = retype(brw_vec1_grf(offset.nr, 0), BRW_REGISTER_TYPE_UD);
+   } else {
+      assert(offset.dw1.ud % 4 == 0); /* Must be DWord aligned */
+   }
+   brw_MOV(p,
+           retype(brw_vec1_reg(BRW_MESSAGE_REGISTER_FILE,
+                               mrf.nr,
+                               2), BRW_REGISTER_TYPE_UD),
+           offset);
+   brw_pop_insn_state(p);
+
+   if (index.file == BRW_IMMEDIATE_VALUE) {
+      assert(index.type == BRW_REGISTER_TYPE_UD);
+      uint32_t surf_index = index.dw1.ud;
+
+      brw_inst *insn = brw_next_insn(p, BRW_OPCODE_SEND);
+      dst = retype(vec8(dst), BRW_REGISTER_TYPE_UW);
+      brw_set_dest(p, insn, dst);
+      brw_set_src0(p, insn, header);
+      brw_set_dp_read_message(p,
+                              insn,
+                              surf_index,
+                              BRW_DATAPORT_OWORD_BLOCK_1_OWORDLOW,
+                              GEN7_DATAPORT_DC_UNALIGNED_OWORD_BLOCK_READ,
+                              BRW_DATAPORT_READ_TARGET_DATA_CACHE,
+                              1, /* mlen */
+                              true, /* header present */
+                              1); /* rlen */
+
+      brw_mark_surface_used(prog_data, surf_index);
+   } else {
+      struct brw_reg addr = vec1(retype(brw_address_reg(0), BRW_REGISTER_TYPE_UD));
+
+      brw_push_insn_state(p);
+      brw_set_default_mask_control(p, BRW_MASK_DISABLE);
+      brw_set_default_access_mode(p, BRW_ALIGN_1);
+
+      /* a0.0 = surf_index & 0xff */
+      brw_inst *insn_and = brw_next_insn(p, BRW_OPCODE_AND);
+      brw_inst_set_exec_size(p->brw, insn_and, BRW_EXECUTE_1);
+      brw_set_dest(p, insn_and, addr);
+      brw_set_src0(p, insn_and, vec1(retype(index, BRW_REGISTER_TYPE_UD)));
+      brw_set_src1(p, insn_and, brw_imm_ud(0x0ff));
+      brw_pop_insn_state(p);
+
+      /* dst = send(payload, a0.0 | <descriptor>) */
+      brw_inst *insn = brw_send_indirect_message(
+         p, GEN7_SFID_DATAPORT_DATA_CACHE, dst, header, addr);
+      brw_set_dp_read_message(p,
+                              insn,
+                              0,
+                              BRW_DATAPORT_OWORD_BLOCK_1_OWORDLOW,
+                              GEN7_DATAPORT_DC_UNALIGNED_OWORD_BLOCK_READ,
+                              BRW_DATAPORT_READ_TARGET_DATA_CACHE,
+                              1, /* mlen */
+                              true, /* header present */
+                              1); /* rlen */
+   }
+}
+
+void
 fs_generator::generate_buffer_store(fs_inst *inst,
                                     struct brw_reg dst,
                                     struct brw_reg src,
@@ -2203,6 +2281,10 @@ fs_generator::generate_code(const cfg_t *cfg, int dispatch_width)
 
       case SHADER_OPCODE_BUFFER_LOAD:
          generate_buffer_load(inst, dst, src[0], src[1]);
+         break;
+
+      case SHADER_OPCODE_UNALIGNED_BUFFER_LOAD:
+         generate_unaligned_buffer_load(inst, dst, src[0], src[1]);
          break;
 
       case SHADER_OPCODE_BUFFER_STORE:
