2194d36772563229ea8cccd9c418741dc3953517
Eduardo Lima Mitev <elima@igalia.com>
2015-04-17 18:10:50 +0200

    i965/nir: Move brw_type_for_nir_type() to brw_nir to allow reuse
    Upcoming NIR->vec4 pass can benefit from this method, so lets move it up.
    
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=89580

--- src/mesa/drivers/dri/i965/brw_nir.h
+++ src/mesa/drivers/dri/i965/brw_nir.h
@@ -24,6 +24,7 @@
 #pragma once
 
 #include "brw_context.h"
+#include "brw_reg.h"
 #include "glsl/nir/nir.h"
 
 #ifdef __cplusplus
@@ -78,6 +79,8 @@ nir_shader *brw_create_nir(struct brw_context *brw,
                            const struct gl_shader_program *shader_prog,
                            const struct gl_program *prog,
                            gl_shader_stage stage);
+
+enum brw_reg_type brw_type_for_nir_type(nir_alu_type type);
 
 #ifdef __cplusplus
 }
