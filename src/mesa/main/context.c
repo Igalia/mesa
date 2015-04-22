4d5ed708a0f6e21f68f1623475cfb873d62f95c2
Samuel Iglesias Gonsalvez <siglesias@igalia.com>
2015-04-22 16:11:13 +0200

    mesa: Add getters for the GL_ARB_shader_storage_buffer_object max constants
    FIXME: find a proper value for MaxShaderStorageBlocks
    
    Signed-off-by: Samuel Iglesias Gonsalvez <siglesias@igalia.com>

--- src/mesa/main/context.c
+++ src/mesa/main/context.c
@@ -553,6 +553,8 @@ init_program_limits(struct gl_constants *consts, gl_shader_stage stage,
 
    prog->MaxAtomicBuffers = 0;
    prog->MaxAtomicCounters = 0;
+
+   prog->MaxShaderStorageBlocks = 12;
 }
 
 
