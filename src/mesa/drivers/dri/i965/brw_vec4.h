86c592aec20a2d7535d85d4b41ec9b558a4e3d35
Eduardo Lima Mitev <elima@igalia.com>
2015-04-23 12:24:41 +0200

    nir-vec4: Add support for direct uniforms
    Built-in and indirect uniforms are still missing, pending a better
    understanding of the current code.

--- src/mesa/drivers/dri/i965/brw_vec4.h
+++ src/mesa/drivers/dri/i965/brw_vec4.h
@@ -396,6 +396,9 @@ public:
    virtual void emit_nir_code();
    virtual void nir_setup_inputs(nir_shader *shader);
    virtual void nir_setup_outputs(nir_shader *shader);
+   virtual void nir_setup_uniforms(nir_shader *shader);
+   virtual void nir_setup_uniform(nir_variable *var);
+   virtual void nir_setup_builtin_uniform(nir_variable *var);
    virtual void nir_emit_system_values (nir_shader *shader);
    virtual void nir_emit_intrinsic(nir_intrinsic_instr *instr);
    virtual void nir_emit_impl(nir_function_impl *impl);
