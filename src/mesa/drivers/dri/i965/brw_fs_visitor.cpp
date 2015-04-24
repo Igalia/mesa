a7c69ea1ca22b2781add39f5925a92a8cb363fc5
Iago Toral Quiroga <itoral@igalia.com>
2015-04-24 09:35:44 +0200

    i965/fs: Implement shader storage buffer object atomic intrinsics

--- src/mesa/drivers/dri/i965/brw_fs_visitor.cpp
+++ src/mesa/drivers/dri/i965/brw_fs_visitor.cpp
@@ -3318,6 +3318,120 @@ fs_visitor::visit_atomic_counter_intrinsic(ir_call *ir)
 }
 
 void
+fs_visitor::visit_atomic_intrinsic(ir_call *ir)
+{
+   /* The first argument to an atomic operation can only be a buffer variable
+    * which at this point must have been lowered by lower_ubo_reference to
+    * a ir_binop_ssbo_load expression. The ir_binop_ssbo_load expression
+    * contains the surface index and offset data we need.
+    */
+   ir_instruction *inst = (ir_instruction *) ir->actual_parameters.get_head();
+   ir_expression *ssbo_expr = inst->as_expression();
+   assert(ssbo_expr);
+
+   ir_constant *const_uniform_block = ssbo_expr->operands[0]->as_constant();
+
+   fs_reg surface;
+   if (const_uniform_block) {
+      unsigned surf_index = stage_prog_data->binding_table.ubo_start +
+                            const_uniform_block->value.u[0];
+      surface = fs_reg(surf_index);
+   } else {
+      ssbo_expr->operands[0]->accept(this);
+      surface = this->result;
+      emit(ADD(surface, surface,
+               fs_reg(stage_prog_data->binding_table.ubo_start)));
+
+      /* Assume this may touch any UBO. This is the same we do for other
+       * UBO/SSBO accesses with non-constant surface.
+       */
+      brw_mark_surface_used(prog_data,
+                            stage_prog_data->binding_table.ubo_start +
+                            shader_prog->NumUniformBlocks - 1);
+   }
+
+   ssbo_expr->operands[1]->accept(this);
+   fs_reg offset = this->result;
+
+   int param_count = ir->actual_parameters.length();
+   assert(param_count == 2 || param_count == 3);
+
+   /* Get data1 parameter (this is always present) */
+   ir_instruction *param = (ir_instruction *) ((exec_node *)inst)->next;
+   assert(param);
+   param->accept(this);
+   fs_reg data1 = this->result;
+
+   /* Emit the actual atomic operation operation */
+   const char *callee = ir->callee->function_name();
+   ir->return_deref->accept(this);
+   fs_reg dst = this->result;
+   if (!strcmp("__intrinsic_atomic_add", callee)) {
+      emit_untyped_atomic(BRW_AOP_ADD, surface, dst, offset,
+                          data1, fs_reg());
+      return;
+   }
+
+   if (!strcmp("__intrinsic_atomic_and", callee)) {
+      emit_untyped_atomic(BRW_AOP_AND, surface, dst, offset,
+                          data1, fs_reg());
+      return;
+   }
+
+   if (!strcmp("__intrinsic_atomic_or", callee)) {
+      emit_untyped_atomic(BRW_AOP_OR, surface, dst, offset,
+                          data1, fs_reg());
+      return;
+   }
+
+   if (!strcmp("__intrinsic_atomic_xor", callee)) {
+      emit_untyped_atomic(BRW_AOP_XOR, surface, dst, offset,
+                          data1, fs_reg());
+      return;
+   }
+
+   if (!strcmp("__intrinsic_atomic_min", callee)) {
+      if (dst.type == BRW_REGISTER_TYPE_D) {
+         emit_untyped_atomic(BRW_AOP_IMIN, surface, dst, offset,
+                             data1, fs_reg());
+      } else {
+         emit_untyped_atomic(BRW_AOP_UMIN, surface, dst, offset,
+                             data1, fs_reg());
+      }
+      return;
+   }
+
+   if (!strcmp("__intrinsic_atomic_max", callee)) {
+      if (dst.type == BRW_REGISTER_TYPE_D) {
+         emit_untyped_atomic(BRW_AOP_IMAX, surface, dst, offset,
+                             data1, fs_reg());
+      } else {
+         emit_untyped_atomic(BRW_AOP_UMAX, surface, dst, offset,
+                             data1, fs_reg());
+      }
+      return;
+   }
+
+   if (!strcmp("__intrinsic_atomic_exchange", callee)) {
+      emit_untyped_atomic(BRW_AOP_MOV, surface, dst, offset,
+                          data1, fs_reg());
+      return;
+   }
+
+   if (!strcmp("__intrinsic_atomic_comp_swap", callee)) {
+      param = (ir_instruction *) ((exec_node *)inst)->next->next;
+      assert(param);
+      param->accept(this);
+      fs_reg data2 = this->result;
+      emit_untyped_atomic(BRW_AOP_CMPWR, surface, dst, offset,
+                          data1, data2);
+      return;
+   }
+
+   unreachable("Unsupported atomic intrinsic");
+}
+
+void
 fs_visitor::visit(ir_call *ir)
 {
    const char *callee = ir->callee->function_name();
@@ -3326,6 +3440,15 @@ fs_visitor::visit(ir_call *ir)
        !strcmp("__intrinsic_atomic_increment", callee) ||
        !strcmp("__intrinsic_atomic_predecrement", callee)) {
       visit_atomic_counter_intrinsic(ir);
+   } else if (!strcmp("__intrinsic_atomic_add", callee) ||
+              !strcmp("__intrinsic_atomic_min", callee) ||
+              !strcmp("__intrinsic_atomic_max", callee) ||
+              !strcmp("__intrinsic_atomic_and", callee) ||
+              !strcmp("__intrinsic_atomic_or", callee) ||
+              !strcmp("__intrinsic_atomic_xor", callee) ||
+              !strcmp("__intrinsic_atomic_exchange", callee) ||
+              !strcmp("__intrinsic_atomic_comp_swap", callee)) {
+      visit_atomic_intrinsic(ir);
    } else {
       unreachable("Unsupported intrinsic.");
    }
