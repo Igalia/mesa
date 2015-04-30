7a3215ef007bd3dec12bffb2c94fc08c9e444170
Francisco Jerez <currojerez@riseup.net>
2015-04-30 19:31:08 +0300

    i965: Define consistent interface to enable instruction conditional modifiers.
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>

--- src/mesa/drivers/dri/i965/brw_ir_vec4.h
+++ src/mesa/drivers/dri/i965/brw_ir_vec4.h
@@ -221,6 +221,17 @@ exec_predicate(enum brw_predicate pred, vec4_instruction *inst)
 {
    return exec_predicate_inv(pred, false, inst);
 }
+
+/**
+ * Write the result of evaluating the condition given by \p mod to a flag
+ * register.
+ */
+inline vec4_instruction *
+exec_condmod(enum brw_conditional_mod mod, vec4_instruction *inst)
+{
+   inst->conditional_mod = mod;
+   return inst;
+}
 } /* namespace brw */
 
 #endif
