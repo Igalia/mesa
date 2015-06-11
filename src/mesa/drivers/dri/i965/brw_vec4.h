711e7bb7a764d3f7e9dacc8e270856150889596d
Eduardo Lima Mitev <elima@igalia.com>
2015-06-11 14:32:28 +0200

    nir-vec4: Add vec4_visitor::nir_gather_channel() similar to gather_channel()
    We should consider factorizing them in the future.

--- src/mesa/drivers/dri/i965/brw_vec4.h
+++ src/mesa/drivers/dri/i965/brw_vec4.h
@@ -411,6 +411,8 @@ public:
    virtual void nir_emit_jump(nir_jump_instr *instr);
    virtual void nir_emit_texture(nir_tex_instr *instr);
 
+   virtual uint32_t nir_gather_channel(unsigned gather_component, uint32_t sampler);
+
    dst_reg get_nir_dest(nir_dest dest);
    src_reg get_nir_src(nir_src src, enum brw_reg_type type);
    src_reg get_nir_src(nir_src src, nir_alu_type type);
