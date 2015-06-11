38dd37ffc5a9895f7912658594fa00ba01cbce97
Eduardo Lima Mitev <elima@igalia.com>
2015-06-11 10:49:52 +0200

    nir-vec4: Overload get_nir_src() to accept a brw_reg_type enum too

--- src/mesa/drivers/dri/i965/brw_vec4.h
+++ src/mesa/drivers/dri/i965/brw_vec4.h
@@ -412,6 +412,7 @@ public:
    virtual void nir_emit_texture(nir_tex_instr *instr);
 
    dst_reg get_nir_dest(nir_dest dest);
+   src_reg get_nir_src(nir_src src, enum brw_reg_type type);
    src_reg get_nir_src(nir_src src, nir_alu_type type);
    src_reg get_nir_src(nir_src src);
 
