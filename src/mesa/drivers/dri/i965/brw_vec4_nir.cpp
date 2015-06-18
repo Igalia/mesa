6c627470d1abcba7b2c640c1d1b552c1e85a63e8
Alejandro Piñeiro <apinheiro@igalia.com>
2015-06-18 11:16:41 +0200

    i965/nir/vec4: Setup LOD source register
    (@TODO: apinheiro to review it)
    
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=89580

--- src/mesa/drivers/dri/i965/brw_vec4_nir.cpp
+++ src/mesa/drivers/dri/i965/brw_vec4_nir.cpp
@@ -1400,6 +1400,7 @@ vec4_visitor::nir_emit_texture(nir_tex_instr *instr)
    const glsl_type *coord_type = NULL;
    src_reg shadow_comparitor;
    int shadow_compare = 0;
+   src_reg lod;
 
    /* Load the texture operation sources */
    for (unsigned i = 0; i < instr->num_srcs; i++) {
@@ -1437,7 +1438,19 @@ vec4_visitor::nir_emit_texture(nir_tex_instr *instr)
          break;
 
       case nir_tex_src_lod:
-         /* @TODO: not yet implemented */
+         switch (instr->op) {
+         case nir_texop_txs:
+            lod = retype(src, BRW_REGISTER_TYPE_UD);
+            break;
+
+         case nir_texop_txf:
+            lod = retype(src, BRW_REGISTER_TYPE_D);
+            break;
+
+         default:
+            lod = retype(src, BRW_REGISTER_TYPE_F);
+            break;
+         }
          break;
 
       case nir_tex_src_ms_index:
