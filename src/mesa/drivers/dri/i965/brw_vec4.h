538cd418b157430c7a797945b466d969973961b3
Iago Toral Quiroga <itoral@igalia.com>
2015-07-09 10:26:15 +0200

    i965/vec4: Implement __intrinsic_store_ssbo

--- src/mesa/drivers/dri/i965/brw_vec4.h
+++ src/mesa/drivers/dri/i965/brw_vec4.h
@@ -403,6 +403,9 @@ public:
 
    void visit_atomic_counter_intrinsic(ir_call *ir);
 
+   void visit_store_ssbo_intrinsic(ir_call *ir);
+
+   int type_size(const struct glsl_type *type);
    bool is_high_sampler(src_reg sampler);
 
    virtual void emit_nir_code();
