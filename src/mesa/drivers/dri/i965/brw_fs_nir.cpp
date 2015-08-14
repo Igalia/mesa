de8b8db64d3fc4f4aab24407b5614d31396176e8
Connor Abbott <connor.w.abbott@intel.com>
2015-08-14 12:29:31 -0700

    i965/fs/nir: translate double pack/unpack

--- src/mesa/drivers/dri/i965/brw_fs_nir.cpp
+++ src/mesa/drivers/dri/i965/brw_fs_nir.cpp
@@ -1036,6 +1036,18 @@ fs_visitor::nir_emit_alu(const fs_builder &bld, nir_alu_instr *instr)
       inst->saturate = instr->dest.saturate;
       break;
 
+   case nir_op_pack_double_2x32_split:
+      bld.emit(FS_OPCODE_PACK, result, op[0], op[1]);
+      break;
+
+   case nir_op_unpack_double_2x32_split_x:
+      bld.MOV(result, stride(retype(op[0], BRW_REGISTER_TYPE_UD), 2));
+      break;
+
+   case nir_op_unpack_double_2x32_split_y:
+      bld.MOV(result, stride(horiz_offset(retype(op[0], BRW_REGISTER_TYPE_UD), 1), 2));
+      break;
+
    case nir_op_fpow:
       inst = bld.emit(SHADER_OPCODE_POW, result, op[0], op[1]);
       inst->saturate = instr->dest.saturate;
