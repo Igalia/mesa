be21672e935c343792db426896e3ef52e028095a
Connor Abbott <connor.w.abbott@intel.com>
2015-08-14 12:20:37 -0700

    nir: add support for (un)pack_double_2x32

--- src/compiler/nir/nir_opcodes.py
+++ src/compiler/nir/nir_opcodes.py
@@ -255,6 +255,34 @@ dst.x = (src0.x <<  0) |
         (src0.w << 24);
 """)
 
+unop_horiz("pack_double_2x32", 1, tfloat64, 2, tuint32, """
+union {
+    double d;
+    struct {
+        uint32_t i1;
+        uint32_t i2;
+    };
+} di;
+
+di.i1 = src0.x;
+di.i2 = src0.y;
+dst.x = di.d;
+""")
+
+unop_horiz("unpack_double_2x32", 2, tuint32, 1, tfloat64, """
+union {
+    double d;
+    struct {
+        uint32_t i1;
+        uint32_t i2;
+    };
+} di;
+
+di.d = src0.x;
+dst.x = di.i1;
+dst.y = di.i2;
+""")
+
 # Lowered floating point unpacking operations.
 
 
