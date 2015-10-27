a82f672a5c7fdc8c1d9f6a32b558c8bd1f6365d2
Eduardo Lima Mitev <elima@igalia.com>
2015-10-27 15:58:59 +0100

    mesa/common: Add a default QueryInternalFormat() function for drivers
    This is a fallback function for drivers not implementing
    ARB_internalformat_query2.
    
    @FIXME: It is now just boilerplate. It should fill the output buffer with
    a meaningful result (most likely GL_NOT_SUPPORTED enum) depending on the
    queried pname.

--- src/mesa/main/formatquery.c
+++ src/mesa/main/formatquery.c
@@ -96,6 +96,20 @@ _mesa_query_samples_for_format(struct gl_context *ctx, GLenum target,
    return 1;
 }
 
+void
+_mesa_query_internal_format_default(struct gl_context *ctx, GLenum target,
+                                    GLenum internalFormat, GLenum pname,
+                                    GLint *params)
+{
+   (void) ctx;
+   (void) target;
+   (void) internalFormat;
+   (void) pname;
+   (void) params;
+
+   /* @TODO */
+}
+
 static bool
 _is_texture_target(GLenum target)
 {
