a81e2620160ad79751f5f961947d828d13c4cce1
Connor Abbott <cwabbott0@gmail.com>
2015-11-01 02:48:41 -0500

    i965/fs: add a pass for lowering PACK opcodes

--- src/mesa/drivers/dri/i965/brw_fs.cpp
+++ src/mesa/drivers/dri/i965/brw_fs.cpp
@@ -5330,6 +5330,11 @@ fs_visitor::optimize()
       OPT(dead_code_eliminate);
    }
 
+   if (OPT(lower_pack)) {
+      OPT(register_coalesce);
+      OPT(dead_code_eliminate);
+   }
+
    OPT(opt_combine_constants);
    OPT(lower_integer_multiplication);
 
