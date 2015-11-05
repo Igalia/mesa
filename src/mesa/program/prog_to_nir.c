01bc4e833df177982bc09ac3e87315b3c9e3e574
Iago Toral Quiroga <itoral@igalia.com>
2015-11-05 11:25:52 +0100

    program/nir: include bit-size information
    Fixes multiple piglit crashes in brw_glsl_base_type_for_nir_type.

--- src/mesa/program/prog_to_nir.c
+++ src/mesa/program/prog_to_nir.c
@@ -607,7 +607,7 @@ ptn_tex(nir_builder *b, nir_alu_dest dest, nir_ssa_def **src,
 
    instr = nir_tex_instr_create(b->shader, num_srcs);
    instr->op = op;
-   instr->dest_type = nir_type_float;
+   instr->dest_type = nir_type_float32;
    instr->is_shadow = prog_inst->TexShadow;
    instr->sampler_index = prog_inst->TexSrcUnit;
 
