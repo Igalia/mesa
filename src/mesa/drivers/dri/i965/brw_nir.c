e3b9967638801a1e74243730f6ecaea3f45d0b60
Iago Toral Quiroga <itoral@igalia.com>
2016-01-04 12:52:36 +0100

    i965/fs: lower trunc() for doubles
    FIXME: We might want to squash this into 'i965: use double lowering pass'

--- src/mesa/drivers/dri/i965/brw_nir.c
+++ src/mesa/drivers/dri/i965/brw_nir.c
@@ -450,7 +450,8 @@ nir_optimize(nir_shader *nir, bool is_scalar)
       OPT(nir_opt_undef);
       OPT_V(nir_lower_doubles, nir_lower_drcp |
                                nir_lower_dsqrt |
-                               nir_lower_drsq);
+                               nir_lower_drsq |
+                               nir_lower_dtrunc);
       OPT_V(nir_lower_double_pack);
    } while (progress);
 
