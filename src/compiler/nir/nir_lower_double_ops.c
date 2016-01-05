5c6593e4d1de5e2c1f1c59361acd7e3dbf5e32d2
Iago Toral Quiroga <itoral@igalia.com>
2016-01-05 09:14:51 +0100

    nir/lower_double_ops: lower fract()
    At least i965 hardware does not have native support for fract() on doubles.

--- src/compiler/nir/nir_lower_double_ops.c
+++ src/compiler/nir/nir_lower_double_ops.c
@@ -460,6 +460,12 @@ lower_ceil(nir_builder *b, nir_ssa_def *src)
                               src));
 }
 
+static nir_ssa_def *
+lower_fract(nir_builder *b, nir_ssa_def *src)
+{
+   return nir_fsub(b, src, nir_ffloor(b, src));
+}
+
 static void
 lower_doubles_instr(nir_alu_instr *instr, nir_lower_doubles_options options)
 {
@@ -498,6 +504,11 @@ lower_doubles_instr(nir_alu_instr *instr, nir_lower_doubles_options options)
          return;
       break;
 
+   case nir_op_ffract:
+      if (!(options & nir_lower_dfract))
+         return;
+      break;
+
    default:
       return;
    }
@@ -529,6 +540,9 @@ lower_doubles_instr(nir_alu_instr *instr, nir_lower_doubles_options options)
       break;
    case nir_op_fceil:
       result = lower_ceil(&bld, src);
+      break;
+   case nir_op_ffract:
+      result = lower_fract(&bld, src);
       break;
    default:
       unreachable("unhandled opcode");
