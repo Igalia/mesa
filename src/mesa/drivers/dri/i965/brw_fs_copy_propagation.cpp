4ac536768e92170c1dc92ba6bacdb6da32d3a9a2
Iago Toral Quiroga <itoral@igalia.com>
2016-01-18 10:44:20 +0100

    i965/fs: Fix copy propagation of load payload for double operands
    Specifically, consider the size of the data type of the operand to compute
    the number of registers written.
    
    v2 (Sam):
    - Fix line width (Jordan).
    - Add an assert (Jordan).
    - Use REG_SIZE in the calculation of regs_written (Curro)
    
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
    Reviewed-by: Francisco Jerez <currojerez@riseup.net>

--- src/mesa/drivers/dri/i965/brw_fs_copy_propagation.cpp
+++ src/mesa/drivers/dri/i965/brw_fs_copy_propagation.cpp
@@ -774,8 +774,10 @@ fs_visitor::opt_copy_propagate_local(void *copy_prop_ctx, bblock_t *block,
                  inst->dst.file == VGRF) {
          int offset = 0;
          for (int i = 0; i < inst->sources; i++) {
+            assert(type_sz(inst->src[i].type) >= 4);
             int effective_width = i < inst->header_size ? 8 : inst->exec_size;
-            int regs_written = effective_width / 8;
+            int regs_written = effective_width / 8 *
+               type_sz(inst->src[i].type) / REG_SIZE;
             if (inst->src[i].file == VGRF) {
                acp_entry *entry = ralloc(copy_prop_ctx, acp_entry);
                entry->dst = inst->dst;
