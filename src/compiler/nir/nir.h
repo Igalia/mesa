ce80e89bb444d3f5b896ec91a38c96e65eca7d4e
Samuel Iglesias Gonsálvez <siglesias@igalia.com>
2016-01-19 08:19:00 +0100

    nir: Add lrp lowering for doubles in opt_algebraic
    Some hardware (i965 on Broadwell generation, for example) does not support
    natively the execution of lrp instruction with double arguments.
    
    Add 'lower_lrp_double' flag to lower this instruction in that case.
    
    Signed-off-by: Samuel Iglesias Gonsálvez <siglesias@igalia.com>

--- src/compiler/nir/nir.h
+++ src/compiler/nir/nir.h
@@ -1554,6 +1554,8 @@ typedef struct nir_shader_compiler_options {
    bool lower_fdiv;
    bool lower_ffma;
    bool lower_flrp;
+   /** Lowers flrp when it does not supports doubles */
+   bool lower_flrp_double;
    bool lower_fpow;
    bool lower_fsat;
    bool lower_fsqrt;
