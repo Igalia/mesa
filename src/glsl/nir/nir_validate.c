db47cb70312901bd25aed170367c0d0183c6c945
Iago Toral Quiroga <itoral@igalia.com>
2016-01-26 09:56:54 +0100

    nir: do not validate bit_sizes for undefined alu sources
    For GLSL code such as this:
    
    buffer SSBO {
       dvec4 d;
    };
    ...
    d.xy = vec4(1, 2, 3, 4).wx;
    
    We generate NIR like this:
    
    vec4 32 ssa_0 = undefined
    vec1 32 ssa_1 = load_const (0x00000000 /* 0.000000 */)
    vec2 64 ssa_2 = load_const (4.000000, 1.000000)
    vec4 64 ssa_3 = vec4 ssa_2, ssa_2.y, ssa_0.z, ssa_0.w
    intrinsic store_ssbo (ssa_3, ssa_1, ssa_1) () (3)
    
    The vec4 operation involves undefined sources which won't be used due
    to the writemask on the ssbo store. However, we still validate all the
    sources for the operation and that involves validating their bit-sizes,
    which will fail since we default to 32-bit and we are doing a 64-bit
    operation.
    
    Just don't validate the bit-size if the source is undefined to prevent
    assertions on code that would otherwise be correct.

--- src/glsl/nir/nir_validate.c
+++ src/glsl/nir/nir_validate.c
@@ -180,9 +180,11 @@ validate_alu_src(nir_alu_instr *instr, unsigned index, validate_state *state)
 
    unsigned num_components;
    unsigned src_bit_size;
+   bool is_undef = false;
    if (src->src.is_ssa) {
       src_bit_size = src->src.ssa->bit_size;
       num_components = src->src.ssa->num_components;
+      is_undef = src->src.ssa->parent_instr->type == nir_instr_type_ssa_undef;
    } else {
       src_bit_size = src->src.reg.reg->bit_size;
       if (src->src.reg.reg->is_packed)
@@ -211,7 +213,7 @@ validate_alu_src(nir_alu_instr *instr, unsigned index, validate_state *state)
          unsigned dest_bit_size =
             instr->dest.dest.is_ssa ? instr->dest.dest.ssa.bit_size
                                     : instr->dest.dest.reg.reg->bit_size;
-         assert(dest_bit_size == src_bit_size);
+         assert(is_undef || dest_bit_size == src_bit_size);
       }
    }
 
