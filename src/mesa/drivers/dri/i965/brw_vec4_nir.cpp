82f83ba5974a6b144df25b6e61319c105b0a65be
Iago Toral Quiroga <itoral@igalia.com>
2016-02-17 10:32:05 +0100

    i965/vec4: Rename DF to/from F generator opcodes
    The opcodes are not specific for conversions to/from float since we need
    the same for conversions to/from other 32-bit types. Rename the opcodes
    accordingly and change the asserts to check the size of the types involved
    instead.

--- src/mesa/drivers/dri/i965/brw_vec4_nir.cpp
+++ src/mesa/drivers/dri/i965/brw_vec4_nir.cpp
@@ -1123,8 +1123,8 @@ vec4_visitor::nir_emit_alu(nir_alu_instr *instr)
 
       dst_reg temp2 = dst_reg(this, glsl_type::dvec4_type);
       temp2 = retype(temp2, BRW_REGISTER_TYPE_F);
-      emit(VEC4_OPCODE_DOUBLE_TO_FLOAT, temp2, src_reg(temp))
-         ->size_written = 2 * REG_SIZE;
+      emit(VEC4_OPCODE_DOUBLE_TO_SINGLE, temp2, src_reg(temp))
+         ->regs_written = 2 * REG_SIZE;
 
       vec4_instruction *inst = emit(MOV(dst, src_reg(temp2)));
       inst->saturate = instr->dest.saturate;
@@ -1135,7 +1135,7 @@ vec4_visitor::nir_emit_alu(nir_alu_instr *instr)
       dst_reg tmp_dst = dst_reg(src_reg(this, glsl_type::dvec4_type));
       src_reg tmp_src = src_reg(this, glsl_type::vec4_type);
       emit(MOV(dst_reg(tmp_src), retype(op[0], BRW_REGISTER_TYPE_F)));
-      emit(VEC4_OPCODE_FLOAT_TO_DOUBLE, tmp_dst, tmp_src);
+      emit(VEC4_OPCODE_SINGLE_TO_DOUBLE, tmp_dst, tmp_src);
       vec4_instruction *inst = emit(MOV(dst, src_reg(tmp_dst)));
       inst->saturate = instr->dest.saturate;
       break;
