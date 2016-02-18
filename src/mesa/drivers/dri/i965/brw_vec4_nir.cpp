eae898b1ec808cc8df19d6de1caa24a189942de7
Samuel Iglesias Gonsálvez <siglesias@igalia.com>
2016-02-18 10:37:23 +0100

    i965/vec4: duplicate bcsel's condition when operating with doubles
    The flag register should have the values in the enabled channels as
    if it was done the CMP with doubles, so SEL works as expected with
    doubles.
    
    Signed-off-by: Samuel Iglesias Gonsálvez <siglesias@igalia.com>

--- src/mesa/drivers/dri/i965/brw_vec4_nir.cpp
+++ src/mesa/drivers/dri/i965/brw_vec4_nir.cpp
@@ -2077,7 +2077,28 @@ vec4_visitor::nir_emit_alu(nir_alu_instr *instr)
    case nir_op_bcsel:
       enum brw_predicate predicate;
       if (!optimize_predicate(instr, &predicate)) {
-         emit(CMP(dst_null_d(), op[0], brw_imm_d(0), BRW_CONDITIONAL_NZ));
+         unsigned num_components = instr->src[0].src.is_ssa ?
+            instr->src[0].src.ssa->num_components :
+            instr->src[0].src.reg.reg->num_components;
+
+         if (op[1].type == BRW_REGISTER_TYPE_DF && num_components == 2) {
+            /* We need to readjust the swizzle of condition that comes from a
+             * boolean: first channel is for the first component of the dvec1/2,
+             * the second channel for the second component of the dvec2. As we
+             * are operating with doubles, they should be duplicated.
+             */
+            dst_reg zero = dst_reg(VGRF, alloc.allocate(1));
+            zero.type = BRW_REGISTER_TYPE_DF;
+            emit(MOV(zero, brw_imm_df(0.0)));
+
+            dst_reg temp = dst_reg(VGRF, alloc.allocate(1));
+            temp.type = BRW_REGISTER_TYPE_DF;
+            op[0].swizzle = BRW_SWIZZLE_XXYY;
+            emit(MOV(temp, retype(op[0], BRW_REGISTER_TYPE_DF)));
+            emit(CMP(dst_null_df(), src_reg(temp), src_reg(zero), BRW_CONDITIONAL_NZ));
+         } else {
+            emit(CMP(dst_null_d(), op[0], brw_imm_d(0), BRW_CONDITIONAL_NZ));
+         }
          switch (dst.writemask) {
          case WRITEMASK_X:
             predicate = BRW_PREDICATE_ALIGN16_REPLICATE_X;
