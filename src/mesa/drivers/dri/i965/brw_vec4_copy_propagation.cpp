8027a0c3e5bd82ea87c0d5e56855a1bfa7efc490
Samuel Iglesias Gonsálvez <siglesias@igalia.com>
2016-03-10 09:18:15 +0100

    i965/vec4: don't copy propagate 64-bit immediates in gen < 8
    They are not supported in gen < 8.
    
    Signed-off-by: Samuel Iglesias Gonsálvez <siglesias@igalia.com>

--- src/mesa/drivers/dri/i965/brw_vec4_copy_propagation.cpp
+++ src/mesa/drivers/dri/i965/brw_vec4_copy_propagation.cpp
@@ -156,6 +156,10 @@ try_constant_propagate(const struct brw_device_info *devinfo,
    if (type_sz(value.type) == 8)
       return false;
 
+   /* 64-bits immediates are not supported in generations older than gen8. */
+   if (devinfo->gen < 8 && type_sz(inst->src[arg].type) == 8)
+      return false;
+
    if (value.type == BRW_REGISTER_TYPE_VF) {
       /* The result of bit-casting the component values of a vector float
        * cannot in general be represented as an immediate.
