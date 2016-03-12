bb7b3e4f6221440f9037868dcd4b07a8de63a8b2
Alejandro Piñeiro <apinheiro@igalia.com>
2016-03-12 12:34:06 +0100

    i965: Enable ARB_vertex_attrib_64bit for gen8+

--- src/mesa/drivers/dri/i965/intel_extensions.c
+++ src/mesa/drivers/dri/i965/intel_extensions.c
@@ -369,6 +369,8 @@ intelInitExtensions(struct gl_context *ctx)
    if (brw->gen >= 8) {
       ctx->Extensions.ARB_stencil_texturing = true;
       ctx->Extensions.ARB_texture_stencil8 = true;
+      ctx->Extensions.ARB_gpu_shader_fp64 = true;
+      ctx->Extensions.ARB_vertex_attrib_64bit = true;
    }
 
    if (brw->gen >= 9) {
