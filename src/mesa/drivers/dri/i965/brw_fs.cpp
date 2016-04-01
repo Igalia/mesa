eeac2cbb3d0ba7ed7e90554e231fa0a9e564c81c
Iago Toral Quiroga <itoral@igalia.com>
2016-04-01 11:54:47 +0200

    i965/fs: do pack lowering early
    We need to do this before our simd lowering pass so that we can
    have gen7 split large writes produced by the pack lowering.
    
    When Connor added this he wanted to do it late so that opt passes
    did not have to know about the writes with stride, but we have been
    dealing with that for a while anyway, and in fact, the original
    code was actually running copy-propagation right after lowering
    pack anyway.
    
    Moving this up does not bring any new issues according to piglit
    and it fixes a lot of fp64 tests that need to split the results of
    pack lowering to work in gen7.

--- src/mesa/drivers/dri/i965/brw_fs.cpp
+++ src/mesa/drivers/dri/i965/brw_fs.cpp
@@ -5469,6 +5469,7 @@ fs_visitor::optimize()
    int iteration = 0;
    int pass_num = 0;
 
+   OPT(lower_pack);
    OPT(lower_simd_width);
    OPT(lower_logical_sends);
 
@@ -5506,11 +5507,6 @@ fs_visitor::optimize()
       split_virtual_grfs();
       OPT(register_coalesce);
       OPT(compute_to_mrf);
-      OPT(dead_code_eliminate);
-   }
-
-   if (OPT(lower_pack)) {
-      OPT(register_coalesce);
       OPT(dead_code_eliminate);
    }
 
