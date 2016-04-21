0e5a36d8060bee34660dfffb41e3a92804934364
Alejandro Piñeiro <apinheiro@igalia.com>
2016-04-21 14:16:18 +0200

    i965/vec4: take into account doubles when creating attribute mapping
    Doubles needs more that one slot per attribute. So when filling the
    attribute_map we check if it is a double in order to allocate one
    extra register.

--- src/mesa/drivers/dri/i965/brw_vs.c
+++ src/mesa/drivers/dri/i965/brw_vs.c
@@ -112,6 +112,7 @@ brw_codegen_vs_prog(struct brw_context *brw,
 
    GLbitfield64 outputs_written = vp->program.Base.OutputsWritten;
    prog_data.inputs_read = vp->program.Base.InputsRead;
+   prog_data.double_inputs_read = vp->program.Base.DoubleInputsRead;
 
    if (key->copy_edgeflag) {
       outputs_written |= BITFIELD64_BIT(VARYING_SLOT_EDGE);
