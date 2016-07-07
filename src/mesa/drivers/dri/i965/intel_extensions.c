53415c0fb98ff189ca4f57e305423df5a9520748
Alejandro Piñeiro <apinheiro@igalia.com>
2016-07-07 11:49:33 +0200

    i965: Enable ARB_vertex_attrib_64bit for Haswell

--- src/mesa/drivers/dri/i965/intel_extensions.c
+++ src/mesa/drivers/dri/i965/intel_extensions.c
@@ -291,8 +291,10 @@ intelInitExtensions(struct gl_context *ctx)
       ctx->Extensions.OES_primitive_bounding_box = true;
       ctx->Extensions.OES_texture_buffer = true;
 
-      if (brw->is_haswell)
+      if (brw->is_haswell) {
          ctx->Extensions.ARB_gpu_shader_fp64 = true;
+         ctx->Extensions.ARB_vertex_attrib_64bit = true;
+      }
 
       if (brw->can_do_pipelined_register_writes) {
          ctx->Extensions.ARB_draw_indirect = true;
