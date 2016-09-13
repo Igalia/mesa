59adf0b51dbb50dfd8f7a299d9b87b3a908528f6
Samuel Iglesias Gonsálvez <siglesias@igalia.com>
2016-09-13 10:41:24 +0200

    REVIEW-HACK i965/vec4/nir: fix GLSL sign() when operating with DF in IVB
    ************************************************************
    
    THIS HACK IS NOT RIGHT. IT IS FIXING THE PROBLEM UPSTREAM BUT NOT IN
    THE PLACE WHERE IT IS HAPPENING -> scalarize_df()
    
                     LOOK FOR ANOTHER SOLUTION!!
    
    ************************************************************
    
    Hacer un fix similar a este en lower_simd_width(), posiblemente
    sea necesario en scalarize_df() para los casos no cubiertos por
    lower_simd_width().
    
    Sería detectar el CMP con destino null (es decir, sólo escribiría
    en el f0) con source DF (¿en Ivybridge sólo?), inyectar este código
    y borrar la instrucción CMP original.
    
    Hablando con Juan creemos que debería estar como parte de
    lower_simd_width() FIJO y, dependiendo de las pruebas, es muy probable
    que sea necesario en scalarize_df() si éste "scalariza) instrucciones
    con destino null:DF.
    
    ************************************************************
    
    To detect if the source is zero, the f0 bits are overwritten due
    to scalarize_df() because it is replicating the instruction but
    using the same flags register.
    
    This patch takes advantage of the work done by scalarize_df() so,
    it saves the CMP result for each component in the same DF
    destination (each DF component of the destination would have 0 if
    the condition is false and ~0 if it is true).
    
    Then convert it to float and do again the CMP but with floats. This
    time, it will set properly each bit of the flags register for all
    the components of both vertices.
    
    Signed-off-by: Samuel Iglesias Gonsálvez <siglesias@igalia.com>

--- src/mesa/drivers/dri/i965/brw_vec4_nir.cpp
+++ src/mesa/drivers/dri/i965/brw_vec4_nir.cpp
@@ -1935,7 +1935,17 @@ vec4_visitor::nir_emit_alu(nir_alu_instr *instr)
           */
 
          /* Check for zero */
-         emit(CMP(dst_null_df(), op[0], setup_imm_df(0.0), BRW_CONDITIONAL_NZ));
+
+         if (devinfo->is_ivybridge) {
+            dst_reg is_nonzero = dst_reg(this, glsl_type::dvec4_type);
+            dst_reg is_nonzero_f = dst_reg(this, glsl_type::vec4_type);
+
+            emit(CMP(is_nonzero, op[0], setup_imm_df(0.0), BRW_CONDITIONAL_NZ));
+            emit_double_to_single(is_nonzero_f, src_reg(is_nonzero), false, BRW_REGISTER_TYPE_F);
+            emit(CMP(dst_null_f(), src_reg(is_nonzero_f), brw_imm_f(0.0f), BRW_CONDITIONAL_NZ));
+         } else {
+            emit(CMP(dst_null_df(), op[0], setup_imm_df(0.0), BRW_CONDITIONAL_NZ));
+         }
 
          /* AND each high 32-bit channel with 0x80000000u */
          dst_reg tmp = dst_reg(this, glsl_type::uvec4_type);
