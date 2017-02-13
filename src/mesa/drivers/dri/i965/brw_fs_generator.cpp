aa878b23c887040b62c077adc7911889a11d35fa
Samuel Iglesias Gonsálvez <siglesias@igalia.com>
2017-02-13 12:09:53 +0100

    i965/fs: emit MOV_INDIRECT with the source with the right register type
    This was hiding bugs as it retyped the source to destination's type.
    
    Signed-off-by: Samuel Iglesias Gonsálvez <siglesias@igalia.com>
    Cc: "17.0" <mesa-stable@lists.freedesktop.org>

--- src/mesa/drivers/dri/i965/brw_fs_generator.cpp
+++ src/mesa/drivers/dri/i965/brw_fs_generator.cpp
@@ -504,7 +504,7 @@ fs_generator::generate_mov_indirect(fs_inst *inst,
 
       struct brw_reg ind_src = brw_VxH_indirect(0, 0);
 
-      brw_inst *mov = brw_MOV(p, dst, retype(ind_src, dst.type));
+      brw_inst *mov = brw_MOV(p, dst, retype(ind_src, reg.type));
 
       if (devinfo->gen == 6 && dst.file == BRW_MESSAGE_REGISTER_FILE &&
           !inst->get_next()->is_tail_sentinel() &&
