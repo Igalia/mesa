b4d82d6d7b4a9aa841d16baec872513b1aa91801
Samuel Iglesias Gonsálvez <siglesias@igalia.com>
2017-04-18 11:58:42 +0200

    Revert "i965/vec4: set uniform DF's width to 4 on IVB/BYT."
    This reverts commit 4600ab1bd59d4d0dec9229d1dde2fe96fe027348.

--- src/intel/compiler/brw_vec4.cpp
+++ src/intel/compiler/brw_vec4.cpp
@@ -1960,9 +1960,7 @@ vec4_visitor::convert_to_hw_regs()
          }
 
          case UNIFORM: {
-            unsigned width = REG_SIZE / 2 / MAX2(4, type_sz(src.type));
-            if (devinfo->gen == 7 && !devinfo->is_haswell)
-               width = 4;
+            const unsigned width = REG_SIZE / 2 / MAX2(4, type_sz(src.type));
             reg = stride(byte_offset(brw_vec4_grf(
                                         prog_data->base.dispatch_grf_start_reg +
                                         src.nr / 2, src.nr % 2 * 4),
