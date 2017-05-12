openharmony/third_party_mesa3d

radeonsi: apply the tess+GS hang workaround to Polaris12 as well
Cc: 17.1 <mesa-stable@lists.freedesktop.org>
Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
Reviewed-by: Nicolai Hähnle <nicolai.haehnle@amd.com>

src/gallium/drivers/radeonsi/si_state_draw.c
@@ -332,7 +332,8 @@ si_get_init_multi_vgt_param(struct si_screen *sscreen,
 				if (sscreen->b.family == CHIP_TONGA ||
 				    sscreen->b.family == CHIP_FIJI ||
 				    sscreen->b.family == CHIP_POLARIS10 ||
-				    sscreen->b.family == CHIP_POLARIS11)
+				    sscreen->b.family == CHIP_POLARIS11 ||
+				    sscreen->b.family == CHIP_POLARIS12)
 					partial_vs_wave = true;
 			} else {
 				partial_vs_wave = true;

Igalia/mesa

(cherry picked from commit ee5908396e4b0d4be64d8bc486d25c2e95b7fd71)
[Andres Gomez: resolve trivial conflicts]
Signed-off-by: Andres Gomez <agomez@igalia.com>
Conflicts:
	src/gallium/drivers/radeonsi/si_state_draw.c

src/gallium/drivers/radeonsi/si_state_draw.c
@@ -319,7 +319,8 @@ static unsigned si_get_ia_multi_vgt_param(struct si_context *sctx,
 				if (sctx->b.family == CHIP_TONGA ||
 				    sctx->b.family == CHIP_FIJI ||
 				    sctx->b.family == CHIP_POLARIS10 ||
-				    sctx->b.family == CHIP_POLARIS11)
+				    sctx->b.family == CHIP_POLARIS11 ||
+				    sctx->b.family == CHIP_POLARIS12)
 					partial_vs_wave = true;
 			} else {
 				partial_vs_wave = true;