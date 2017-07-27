bfaf401d6bc340e28c39d693eb1b1f3ff6bbc1c4
Dave Airlie <airlied@redhat.com>
2017-07-27 04:51:48 +0100

    radv: for stencil only set Z tile mode index to same value
    On SI this was causing a hang in
    dEQP-VK.pipeline.render_to_image.core.2d_array.mipmap.r16g16_sint_s8_uint
    
    This was due to not handling the tile mode index for depth like
    I fixed previously for new GPUs.
    
    Fixes: 01d0c5a9 (radv: fix stencil regression since new addrlib import)
    Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
    Signed-off-by: Dave Airlie <airlied@redhat.com>
    (cherry picked from commit 800d1622096ca52b955bdfc20eb770b80ef15221)
    [Emil Velikov: XXX]
    Signed-off-by: Emil Velikov <emil.velikov@collabora.com>
    
    Conflicts:
    	src/amd/vulkan/radv_device.c

--- src/amd/vulkan/radv_device.c
+++ src/amd/vulkan/radv_device.c
@@ -2843,6 +2843,8 @@ radv_initialise_ds_surface(struct radv_device *device,
 		ds->db_z_info |= S_028040_TILE_MODE_INDEX(tile_mode_index);
 		tile_mode_index = si_tile_mode_index(iview->image, level, true);
 		ds->db_stencil_info |= S_028044_TILE_MODE_INDEX(tile_mode_index);
+		if (stencil_only)
+			ds->db_z_info |= S_028040_TILE_MODE_INDEX(tile_mode_index);
 	}
 
 	if (iview->image->surface.htile_size && !level) {
