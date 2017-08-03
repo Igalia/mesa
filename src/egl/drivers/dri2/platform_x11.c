6fa40b1643df3045b428c8b78a6937ee13299d83
Emil Velikov <emil.velikov@collabora.com>
2017-08-03 19:29:27 +0100

    egl/x11/dri3: adding missing __DRI_BACKGROUND_CALLABLE extension
    Fixes: 3b7b6adf3ac ("egl: Implement __DRI_BACKGROUND_CALLABLE")
    Cc: Timothy Arceri <tarceri@itsqueeze.com>
    Signed-off-by: Emil Velikov <emil.velikov@collabora.com>
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    (cherry picked from commit f24bc1816242895d0bc0711a50bd2dfd3cb8b21a)

--- src/egl/drivers/dri2/platform_x11.c
+++ src/egl/drivers/dri2/platform_x11.c
@@ -1334,6 +1334,7 @@ static const __DRIextension *dri3_image_loader_extensions[] = {
    &dri3_image_loader_extension.base,
    &image_lookup_extension.base,
    &use_invalidate.base,
+   &background_callable_extension.base,
    NULL,
 };
 
