1d497df0a51b4337d3a61d4e84cebb7980b2006c
Andres Gomez <agomez@igalia.com>
2017-10-03 10:58:17 +0300

    Remove wayland-egl.h dep and add function prototypes

--- src/egl/wayland/wayland-egl/wayland-egl-backend.h
+++ src/egl/wayland/wayland-egl/wayland-egl-backend.h
@@ -56,6 +56,22 @@ struct wl_egl_window {
 	struct wl_surface *surface;
 };
 
+struct wl_egl_window *
+wl_egl_window_create(struct wl_surface *surface,
+		     int width, int height);
+
+void
+wl_egl_window_destroy(struct wl_egl_window *egl_window);
+
+void
+wl_egl_window_resize(struct wl_egl_window *egl_window,
+		     int width, int height,
+		     int dx, int dy);
+
+void
+wl_egl_window_get_attached_size(struct wl_egl_window *egl_window,
+				int *width, int *height);
+
 #ifdef  __cplusplus
 }
 #endif
