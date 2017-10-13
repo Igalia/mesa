ead5cc6e836049be6e3a3e8b25f03b4b8a414077
Alejandro Piñeiro <apinheiro@igalia.com>
2017-10-13 16:17:51 +0200

    spirv_extensions: add list of extensions and to_string method
    Ideally this should be generated somehow. One option would be gather
    all the extension dependencies listed on the core grammar, but there
    would be the possibility of not including some of the extensions.
    
    Note that spirv-tools is doing it just slightly better, as it has a
    hardcoded list of extensions manually took from the registry, that
    they parse to get the enum and the to_string method (see
    generate_grammar_tables.py).
    
    v2:
      * Use a macro to improve readability. (Tapani Pälli)
      * Add unreachable on the switch, no default (Eric Engestrom)
      * No typedef enum (Ian Romanick)
      * Sort extensions names (Ian Romanick)
      * Don't add extensions unlikely to be supported by Mesa at any point
        (Ian Romanick)
    
    v3: Include meson.build entries for spirv_extensions.[c|h] that were
       dropped by accident during rebase.

---
+++ src/compiler/spirv/spirv_extensions.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright © 2017 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#ifndef _SPIRV_EXTENSIONS_H_
+#define _SPIRV_EXTENSIONS_H_
+
+#include "compiler/nir/nir.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum SpvExtension {
+   SPV_KHR_16bit_storage = 0,
+   SPV_KHR_device_group,
+   SPV_KHR_multiview,
+   SPV_KHR_shader_ballot,
+   SPV_KHR_shader_draw_parameters,
+   SPV_KHR_storage_buffer_storage_class,
+   SPV_KHR_subgroup_vote,
+   SPV_KHR_variable_pointers,
+   SPV_EXTENSIONS_COUNT
+};
+
+const char *spirv_extensions_to_string(enum SpvExtension ext);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SPIRV_EXTENSIONS */
