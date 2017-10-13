c4ffd60cf3280b0e6c49a5b2b4e5f461b0fe8a78
Eduardo Lima Mitev <elima@igalia.com>
2017-10-13 10:57:31 +0200

    nir/linker: Implement nir_link_uniforms()
    This patch includes initial support for linking uniforms from NIR
    shaders. It is far from complete, but it should handle simple cases of
    uniforms, array uniforms, uniform blocks, samplers and images.

--- src/compiler/nir/nir_linking_helpers.c
+++ src/compiler/nir/nir_linking_helpers.c
@@ -25,6 +25,7 @@
 #include "util/set.h"
 #include "util/hash_table.h"
 #include "nir_linker.h"
+#include "compiler/glsl/ir_uniform.h" /* for gl_uniform_storage */
 
 /* This file contains various little helpers for doing simple linking in
  * NIR.  Eventually, we'll probably want a full-blown varying packing
@@ -505,9 +506,349 @@ nir_compact_varyings(nir_shader *producer, nir_shader *consumer,
                       default_to_smooth_interp);
 }
 
+#define UNMAPPED_UNIFORM_LOC ~0u
+
+static void
+linker_error(struct gl_shader_program *prog, const char *fmt, ...)
+{
+   va_list ap;
+
+   ralloc_strcat(&prog->data->InfoLog, "error: ");
+   va_start(ap, fmt);
+   ralloc_vasprintf_append(&prog->data->InfoLog, fmt, ap);
+   va_end(ap);
+
+   prog->data->LinkStatus = linking_failure;
+}
+
+static void
+nir_setup_uniform_remap_tables(struct gl_context *ctx,
+                               struct gl_shader_program *prog)
+{
+   prog->UniformRemapTable = rzalloc_array(prog,
+                                           struct gl_uniform_storage *,
+                                           prog->NumUniformRemapTable);
+   union gl_constant_value *data =
+      rzalloc_array(prog->data,
+                    union gl_constant_value, prog->data->NumUniformDataSlots);
+   if (!prog->UniformRemapTable || !data) {
+      linker_error(prog, "Out of memory during linking.\n");
+      return;
+   }
+   prog->data->UniformDataSlots = data;
+
+   unsigned data_pos = 0;
+
+   /* Reserve all the explicit locations of the active uniforms. */
+   for (unsigned i = 0; i < prog->data->NumUniformStorage; i++) {
+      struct gl_uniform_storage *uniform = &prog->data->UniformStorage[i];
+
+      if (prog->data->UniformStorage[i].remap_location == UNMAPPED_UNIFORM_LOC)
+         continue;
+
+      /* How many new entries for this uniform? */
+      const unsigned entries = MAX2(1, uniform->array_elements);
+      unsigned num_slots = glsl_get_components(uniform->type);
+
+      uniform->storage = &data[data_pos];
+
+      /* Set remap table entries point to correct gl_uniform_storage. */
+      for (unsigned j = 0; j < entries; j++) {
+         unsigned element_loc = uniform->remap_location + j;
+         prog->UniformRemapTable[element_loc] = uniform;
+
+         data_pos += num_slots;
+      }
+   }
+
+   /* Reserve locations for rest of the uniforms. */
+   for (unsigned i = 0; i < prog->data->NumUniformStorage; i++) {
+      struct gl_uniform_storage *uniform = &prog->data->UniformStorage[i];
+
+      if (uniform->is_shader_storage)
+         continue;
+
+      /* Built-in uniforms should not get any location. */
+      if (uniform->builtin)
+         continue;
+
+      /* Explicit ones have been set already. */
+      if (uniform->remap_location != UNMAPPED_UNIFORM_LOC)
+         continue;
+
+      /* How many new entries for this uniform? */
+      const unsigned entries = MAX2(1, uniform->array_elements);
+
+      /* @FIXME: By now, we add un-assigned unassigned uniform locations
+       * to the end of the uniform file. We need to keep track of empty
+       * locations and use them.
+       */
+      unsigned chosen_location = prog->NumUniformRemapTable;
+
+      /* resize remap table to fit new entries */
+      prog->UniformRemapTable =
+         reralloc(prog,
+                  prog->UniformRemapTable,
+                  struct gl_uniform_storage *,
+                  prog->NumUniformRemapTable + entries);
+      prog->NumUniformRemapTable += entries;
+
+      /* set the base location in remap table for the uniform */
+      uniform->remap_location = chosen_location;
+
+      unsigned num_slots = glsl_get_components(uniform->type);
+
+      uniform->storage = &data[data_pos];
+
+      /* Set remap table entries point to correct gl_uniform_storage. */
+      for (unsigned j = 0; j < entries; j++) {
+         unsigned element_loc = uniform->remap_location + j;
+         prog->UniformRemapTable[element_loc] = uniform;
+
+         data_pos += num_slots;
+      }
+   }
+}
+
+static struct gl_uniform_storage *
+find_previous_uniform_storage(struct gl_shader_program *prog,
+                              int location)
+{
+   for (unsigned i = 0; i < prog->data->NumUniformStorage; i++)
+      if (prog->data->UniformStorage[i].remap_location == location)
+         return &prog->data->UniformStorage[i];
+
+   return NULL;
+}
+
+struct nir_link_uniforms_state {
+   /* per-whole program */
+   unsigned num_hidden_uniforms;
+   unsigned num_values;
+   unsigned max_uniform_location;
+   unsigned shader_samplers_used;
+   unsigned shader_shadow_samplers;
+   unsigned next_sampler_index;
+   unsigned next_image_index;
+
+   /* per-shader stage */
+   unsigned num_shader_samplers;
+   unsigned num_shader_images;
+   unsigned num_shader_uniform_components;
+
+   const nir_variable *current_var;
+};
+
+static bool
+nir_link_uniform (struct gl_context *ctx,
+                  struct gl_shader_program *prog,
+                  struct gl_program *stage_program,
+                  gl_shader_stage stage,
+                  const struct glsl_type *type,
+                  const char *name,
+                  int location,
+                  struct nir_link_uniforms_state *state)
+{
+   struct gl_uniform_storage *uniform = NULL;
+   if (glsl_type_is_struct(type)) {
+      for (unsigned i = 0; i < glsl_get_length(type); i++) {
+         const struct glsl_type *field_type = glsl_get_struct_field(type, i);
+
+         const char *field_name = glsl_get_struct_elem_name(type, i);
+         char *uniform_name = NULL;
+         if (name)
+            asprintf(&uniform_name, "%s.%s", name, field_name);
+         else
+            uniform_name = strdup(field_name);
+
+         unsigned entries = MAX2(1, glsl_get_length(field_type));
+         if (!nir_link_uniform(ctx, prog, stage_program, stage,
+                               field_type, uniform_name, location,
+                               state)) {
+            return false;
+         }
+
+         location += entries;
+         free(uniform_name);
+      }
+   } else {
+      /* Create a new uniform storage entry */
+      prog->data->UniformStorage =
+         reralloc(prog->data,
+                  prog->data->UniformStorage,
+                  struct gl_uniform_storage,
+                  prog->data->NumUniformStorage + 1);
+      if (!prog->data->UniformStorage) {
+         linker_error(prog, "Out of memory during linking.\n");
+         return false;
+      }
+
+      uniform = &prog->data->UniformStorage[prog->data->NumUniformStorage];
+      prog->data->NumUniformStorage++;
+
+      /* Initialize its members */
+      memset(uniform, 0x00, sizeof(struct gl_uniform_storage));
+      uniform->name = ralloc_strdup(prog, name ? name : "");
+
+      const struct glsl_type *type_no_array = glsl_without_array(type);
+      if (glsl_type_is_array(type))
+         uniform->type = type_no_array;
+      else
+         uniform->type = type;
+      uniform->array_elements = glsl_get_length(type);
+      uniform->active_shader_mask |= 1 << stage;
+      if (location >= 0) {
+         /* Uniform has an explicit location */
+         uniform->remap_location = location;
+      } else {
+         uniform->remap_location = UNMAPPED_UNIFORM_LOC;
+      }
+
+      /* @FIXME: Pending to initialize the following members */
+      uniform->block_index = -1;
+      uniform->offset = -1;
+      uniform->matrix_stride = -1;
+      uniform->array_stride = -1;
+      uniform->row_major = false;
+      uniform->hidden = false;
+      uniform->builtin = false;
+      uniform->is_shader_storage = false;
+      uniform->atomic_buffer_index = -1;
+      uniform->num_compatible_subroutines = 0;
+      uniform->top_level_array_size = 0;
+      uniform->top_level_array_stride = 0;
+      uniform->is_bindless = false;
+
+      unsigned entries = MAX2(1, uniform->array_elements);
+
+      if (glsl_type_is_sampler(type_no_array)) {
+         /* @FIXME: sampler_index should match that of the same sampler
+          * uniform in other shaders. This means we need to match sampler
+          * uniforms by location (GLSL does it by variable name, but we
+          * want to avoid that).
+          */
+         int sampler_index = state->next_sampler_index;
+         state->next_sampler_index += entries;
+
+         state->num_shader_samplers++;
+
+         uniform->opaque[stage].active = true;
+         uniform->opaque[stage].index = sampler_index;
+
+         const unsigned shadow = glsl_sampler_type_is_shadow(type_no_array);
+
+         for (unsigned i = sampler_index;
+              i < MIN2(state->next_sampler_index, MAX_SAMPLERS);
+              i++) {
+            stage_program->sh.SamplerTargets[i] =
+               glsl_get_sampler_target(type_no_array);
+            state->shader_samplers_used |= 1U << i;
+            state->shader_shadow_samplers |= shadow << i;
+         }
+      } else if (glsl_type_is_image(type_no_array)) {
+         /* @FIXME: image_index should match that of the same image
+          * uniform in other shaders. This means we need to match image
+          * uniforms by location (GLSL does it by variable name, but we
+          * want to avoid that).
+          */
+         int image_index = state->next_image_index;
+         state->next_image_index += entries;
+
+         state->num_shader_images++;
+
+         uniform->opaque[stage].active = true;
+         uniform->opaque[stage].index = image_index;
+
+         /* Set image access qualifiers */
+         const GLenum access =
+            (state->current_var->data.image.read_only ? GL_READ_ONLY :
+             state->current_var->data.image.write_only ? GL_WRITE_ONLY :
+             GL_READ_WRITE);
+         for (unsigned i = image_index;
+              i < MIN2(state->next_image_index, MAX_IMAGE_UNIFORMS);
+              i++) {
+            stage_program->sh.ImageAccess[i] = access;
+         }
+      }
+
+      unsigned values = glsl_get_component_slots(type);
+      state->num_shader_uniform_components += values;
+      state->num_values += values;
+
+      if (state->max_uniform_location < uniform->remap_location + entries)
+         state->max_uniform_location = uniform->remap_location + entries;
+   }
+
+   return true;
+}
+
 void
 nir_link_uniforms(struct gl_context *ctx,
                   struct gl_shader_program *prog)
 {
-   /* This is boilerplate for now */
+   /* First free up any previous UniformStorage items */
+   ralloc_free(prog->data->UniformStorage);
+   prog->data->UniformStorage = NULL;
+   prog->data->NumUniformStorage = 0;
+
+   /* Iterate through all linked shaders */
+   struct nir_link_uniforms_state state = {0,};
+
+   for (unsigned i = 0; i < MESA_SHADER_STAGES; i++) {
+      struct gl_linked_shader *sh = prog->_LinkedShaders[i];
+      if (!sh)
+         continue;
+
+      nir_shader *nir = sh->Program->nir;
+      assert(nir);
+
+      state.num_shader_samplers = 0;
+      state.num_shader_images = 0;
+      state.num_shader_uniform_components = 0;
+
+      nir_foreach_variable(var, &nir->uniforms) {
+         struct gl_uniform_storage *uniform = NULL;
+
+         /* In this stage we only care for uniforms with explicit locations. */
+         if (var->data.location == -1)
+            continue;
+
+         /* Check if the uniform has been processed already for
+          * other stage. If so, validate they are compatible and update
+          * the active stage mask.
+          */
+         uniform = find_previous_uniform_storage(prog, var->data.location);
+         if (uniform) {
+            /* @FIXME: Perform compatibility checks between the uniforms of
+             * this stage and the one processed earlier.
+             */
+
+            uniform->active_shader_mask |= 1 << i;
+
+            continue;
+         }
+
+         state.current_var = var;
+
+         if (!nir_link_uniform(ctx, prog, sh->Program, i, var->type,
+                               var->name, var->data.location, &state)) {
+            return;
+         }
+      }
+
+      sh->Program->SamplersUsed = state.shader_samplers_used;
+      sh->shadow_samplers = state.shader_shadow_samplers;
+      sh->Program->info.num_textures = state.num_shader_samplers;
+      sh->Program->info.num_images = state.num_shader_images;
+      sh->num_uniform_components = state.num_shader_uniform_components;
+      sh->num_combined_uniform_components = sh->num_uniform_components;
+   }
+
+   /* @TODO: Now process all the uniform with unspecified location (-1). */
+
+   prog->data->NumHiddenUniforms = state.num_hidden_uniforms;
+   prog->NumUniformRemapTable = state.max_uniform_location;
+   prog->data->NumUniformDataSlots = state.num_values;
+
+   nir_setup_uniform_remap_tables(ctx, prog);
 }
