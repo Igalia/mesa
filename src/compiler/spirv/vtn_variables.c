904162161bfbe9437717fd442ca507d8e28de4fe
Alejandro Piñeiro <apinheiro@igalia.com>
2017-11-17 15:03:22 +0100

    spirv/nir: translate uniform blocks
    They are supported by SPIR-V for ARB_gl_spirv.

--- src/compiler/spirv/vtn_variables.c
+++ src/compiler/spirv/vtn_variables.c
@@ -1434,7 +1434,7 @@ vtn_storage_class_to_mode(struct vtn_builder *b,
    case SpvStorageClassUniform:
       if (interface_type->block) {
          mode = vtn_variable_mode_ubo;
-         nir_mode = 0;
+         nir_mode = nir_var_uniform;
       } else if (interface_type->buffer_block) {
          mode = vtn_variable_mode_ssbo;
          nir_mode = 0;
@@ -1613,6 +1613,7 @@ vtn_create_variable(struct vtn_builder *b, struct vtn_value *val,
    case vtn_variable_mode_sampler:
    case vtn_variable_mode_uniform:
    case vtn_variable_mode_workgroup:
+   case vtn_variable_mode_ubo:
       /* For these, we create the variable normally */
       var->var = rzalloc(b->shader, nir_variable);
       var->var->name = ralloc_strdup(var->var, val->name);
@@ -1727,7 +1728,6 @@ vtn_create_variable(struct vtn_builder *b, struct vtn_value *val,
    case vtn_variable_mode_param:
       vtn_fail("Not created through OpVariable");
 
-   case vtn_variable_mode_ubo:
    case vtn_variable_mode_ssbo:
    case vtn_variable_mode_push_constant:
       /* These don't need actual variables. */
