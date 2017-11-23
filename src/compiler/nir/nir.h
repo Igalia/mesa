83c85b28a4905e84c56f37b9d561fa372d11b0ea
Eric Engestrom <eric.engestrom@imgtec.com>
2017-11-23 13:16:43 +0000

    compiler: use NDEBUG to guard asserts
    nir_validate.c's #endif already had the correct NDEBUG comment
    
    Fixes: dcb1acdea00a8f2c29777 "nir/validate: Only build in debug mode"
    Fixes: 9ff71b649b4b3808a9e17 "i965/nir: Validate that NIR passes call nir_metadata_preserve()"
    Signed-off-by: Eric Engestrom <eric.engestrom@imgtec.com>
    Reviewed-by: Matt Turner <mattst88@gmail.com>
    (cherry picked from commit 7b85b9b8773b119360a31b66b321ae560a77cb6d)
    Signed-off-by: Emil Velikov <emil.velikov@collabora.com>
    
    Conflicts:
    	src/compiler/nir/nir.h

--- src/compiler/nir/nir.h
+++ src/compiler/nir/nir.h
@@ -41,9 +41,9 @@
 #include "compiler/shader_info.h"
 #include <stdio.h>
 
-#ifdef DEBUG
+#ifndef NDEBUG
 #include "util/debug.h"
-#endif /* DEBUG */
+#endif /* NDEBUG */
 
 #include "nir_opcodes.h"
 
@@ -2298,7 +2298,7 @@ nir_variable *nir_variable_clone(const nir_variable *c, nir_shader *shader);
 nir_deref *nir_deref_clone(const nir_deref *deref, void *mem_ctx);
 nir_deref_var *nir_deref_var_clone(const nir_deref_var *deref, void *mem_ctx);
 
-#ifdef DEBUG
+#ifndef NDEBUG
 void nir_validate_shader(nir_shader *shader);
 void nir_metadata_set_validation_flag(nir_shader *shader);
 void nir_metadata_check_validation_flag(nir_shader *shader);
@@ -2328,7 +2328,7 @@ static inline void nir_metadata_set_validation_flag(nir_shader *shader) { (void)
 static inline void nir_metadata_check_validation_flag(nir_shader *shader) { (void) shader; }
 static inline bool should_clone_nir(void) { return false; }
 static inline bool should_print_nir(void) { return false; }
-#endif /* DEBUG */
+#endif /* NDEBUG */
 
 #define _PASS(nir, do_pass) do {                                     \
    do_pass                                                           \
