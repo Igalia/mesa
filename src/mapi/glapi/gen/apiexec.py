c0bf7409ec8e36634390daa05cb677f296f0bf45
Ian Romanick <ian.d.romanick@intel.com>
2017-12-18 14:44:51 -0800

    mapi: Only expose glSpecializeShader in core profile
    Previously, dispatch_sanity (correctly) checked that
    glSpecializeShader was only available in core profile, but the
    dispatch_sanity test failed.  Commit e5d8ffd fixed this by adding
    glSpecializeShader to common_desktop_functions_possible.  Because I
    wasn't paying enough attention, I reviewed that patch so that
    dispatch_sanity would stop failing.
    
    The correct fix is to not put glSpecializeShader in the non-core
    dispatch tables.
    
    Signed-off-by: Ian Romanick <ian.d.romanick@intel.com>
    (cherry picked from commit ac40e3ad563cdffcbbbfb39246ad764b8381676a)
    (cherry picked from commit b5230226f50c238c85c38e21b5516460ba06b77e)
    (cherry picked from commit 2203d68d097d13eae63ecca9b7583302adfe3c0d)

--- src/mapi/glapi/gen/apiexec.py
+++ src/mapi/glapi/gen/apiexec.py
@@ -291,4 +291,7 @@ functions = {
 
     # GL_ARB_bindless_texture
     "GetVertexAttribLui64vARB": exec_info(compatibility=30, core=31),
+
+    # OpenGL 4.6 / GL_ARB_gl_spirv
+    "SpecializeShaderARB": exec_info(core=33),
 }
