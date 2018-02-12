4723cfd91dfdae20dbe7d6f001b22918e8f6a98f
Alejandro Piñeiro <apinheiro@igalia.com>
2018-02-12 15:50:18 +0100

    nir/linker: fill is_shader_storage for uniforms
    (cherry picked from commit 9e1de000b02be9ef5becb0eba2aacfb52b8e412b)

--- src/compiler/glsl/gl_nir_link_uniforms.c
+++ src/compiler/glsl/gl_nir_link_uniforms.c
@@ -272,6 +272,12 @@ get_next_index(struct nir_link_uniforms_state *state,
 }
 
 
+static bool
+_var_is_ssbo(nir_variable *var)
+{
+   return (var->data.mode == nir_var_shader_storage);
+}
+
 /**
  * Creates the neccessary entries in UniformStorage for the uniform. Returns
  * the number of locations used or -1 on failure.
@@ -369,6 +375,8 @@ nir_link_uniform(struct gl_context *ctx,
       if (uniform->hidden)
          state->num_hidden_uniforms++;
 
+      uniform->is_shader_storage = _var_is_ssbo(state->current_var);
+
       /* @FIXME: the initialization of the following will be done as we
        * implement support for their specific features, like SSBO, atomics,
        * etc.
@@ -379,7 +387,6 @@ nir_link_uniform(struct gl_context *ctx,
       uniform->array_stride = -1;
       uniform->row_major = false;
       uniform->builtin = false;
-      uniform->is_shader_storage = false;
       uniform->atomic_buffer_index = -1;
       uniform->top_level_array_size = 0;
       uniform->top_level_array_stride = 0;
