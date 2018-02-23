057d9d281a90284b0bc68404697679983c7e84eb
Neil Roberts <nroberts@igalia.com>
2018-02-23 17:07:52 +0100

    spirv: Set nir_variable->explicit_binding
    When SpvDecorationBinding is encountered in the SPIR-V source it now
    sets explicit_binding on the nir_variable. This will be used to
    determine whether to initialise sampler and image uniforms with the
    binding value.
    
    (cherry picked from commit 6404a25cdb0e45a119d8dd09c76b1eb52c8ce06b)

--- src/compiler/spirv/vtn_variables.c
+++ src/compiler/spirv/vtn_variables.c
@@ -1503,6 +1503,7 @@ var_decoration_cb(struct vtn_builder *b, struct vtn_value *val, int member,
    switch (dec->decoration) {
    case SpvDecorationBinding:
       vtn_var->binding = dec->literals[0];
+      vtn_var->explicit_binding = true;
       return;
    case SpvDecorationDescriptorSet:
       vtn_var->descriptor_set = dec->literals[0];
@@ -1926,6 +1927,7 @@ vtn_create_variable(struct vtn_builder *b, struct vtn_value *val,
        * for these. We should fix that.
        */
       var->var->data.binding = var->binding;
+      var->var->data.explicit_binding = var->explicit_binding;
       var->var->data.descriptor_set = var->descriptor_set;
       var->var->data.index = var->input_attachment_index;
 
