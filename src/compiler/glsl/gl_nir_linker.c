752c78cbde08fb4ce30c01e8e87068866b204656
Alejandro Piñeiro <apinheiro@igalia.com>
2018-03-23 12:35:48 +0100

    nir/linker: add ubo/ssbo to the program resource list
    (cherry picked from commit 4d94044900e759ce3430a9c9d969e9d404b27ec0)

--- src/compiler/glsl/gl_nir_linker.c
+++ src/compiler/glsl/gl_nir_linker.c
@@ -65,6 +65,18 @@ nir_build_program_resource_list(struct gl_context *ctx,
    }
 
 
+   for (unsigned i = 0; i < prog->data->NumUniformBlocks; i++) {
+      if (!link_util_add_program_resource(prog, resource_set, GL_UNIFORM_BLOCK,
+                                          &prog->data->UniformBlocks[i], 0))
+         return;
+   }
+
+   for (unsigned i = 0; i < prog->data->NumShaderStorageBlocks; i++) {
+      if (!link_util_add_program_resource(prog, resource_set, GL_SHADER_STORAGE_BLOCK,
+                                          &prog->data->ShaderStorageBlocks[i], 0))
+         return;
+   }
+
    _mesa_set_destroy(resource_set, NULL);
 }
 
