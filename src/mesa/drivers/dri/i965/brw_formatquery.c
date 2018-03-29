dfef7c4979a658fc5835aed9435922fb43dadea0
Eleni Maria Stea <estea@igalia.com>
2018-03-29 15:28:48 +0300

    Fixes the samples query for Gen7
    On Gen7: in function brw_initalize_context_constants we set the
    max_samples to 4 for all formats except of the depth formats but
    the function brw_query_samples_for_format returns 4 for every
    format that has more than 8 bytes.
    
    Changes:
    We perform the check for the 8bytes but only for the GLES case.
    
    In OpenGL:
    The brw_query_samples_for_format now checks if the format is depth or
    stencil. If it isn't it returns 4. If it is, it returns
    MaxDepthSamples (=8) and 4.
    
    We check if the format is a depth format so that we are in consistence
    with the brw_initalize_context_constants function. We check if the
    format is a stencil format because The max GL_SAMPLES for
    GL_STENCIL_INDEX must be greater than or equal to
    GL_MAX_DEPTH_TEXTURE_SAMPLES.
    
    Fixes:
    The following vk-gl-cts:
    1) GTF-GL46.gtf21.GL3Tests.proxy_textures.proxy_textures_invalid_samples
    2) GTF-GL46.gtf44.GL31Tests.texture_stencil8.texture_stencil8_gl44
    3) dEQP-GLES31.functional.state_query.internal_format.renderbuffer.stencil_index8_samples
    
    (cherry picked from commit 4020d89b6b27ed8817216dea5523f9ea48035a1d)
    (cherry picked from commit 7cf69639460c0dd16fa2f2a5ba3f8629b17b199a)

--- src/mesa/drivers/dri/i965/brw_formatquery.c
+++ src/mesa/drivers/dri/i965/brw_formatquery.c
@@ -54,18 +54,18 @@ brw_query_samples_for_format(struct gl_context *ctx, GLenum target,
       return 3;
 
    case 7: {
-      mesa_format format =
-         ctx->Driver.ChooseTextureFormat(ctx, GL_TEXTURE_2D, internalFormat,
-                                         GL_NONE, GL_NONE);
-
-      if (_mesa_get_format_bytes(format) > 8) {
-         samples[0] = 4;
-         return 1;
-      } else {
-         samples[0] = 8;
-         samples[1] = 4;
-         return 2;
+      if (!_mesa_is_gles(ctx)) {
+         mesa_format format = _mesa_base_tex_format(ctx, internalFormat);
+         if (format != -1) {
+            if (_mesa_is_depth_or_stencil_format(format)) {
+               samples[0] = 8;
+               samples[1] = 4;
+               return 2;
+            }
+         }
       }
+      samples[0] = 4;
+      return 1;
    }
 
    case 6:
