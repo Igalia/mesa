4c535f33aeeedc547d5da0dbf37eb5350c1642c8
Daniel Schürmann <daniel.schuermann@campus.tu-berlin.de>
2018-04-06 17:09:01 +0200

    aco: assembler emits vector<uint32_t> instead of iostream

--- src/amd/compiler/aco_assembler.cpp
+++ src/amd/compiler/aco_assembler.cpp
@@ -1,4 +1,4 @@
-#include <fstream>
+#include <vector>
 
 #include "aco_ir.h"
 
@@ -9,10 +9,8 @@ struct asm_context {
    // and, when emitting the block, correct the offset in instr
 };
 
-void emit_instruction(asm_context ctx, std::ofstream& out, Instruction* instr)
+void emit_instruction(asm_context ctx, std::vector<uint32_t>& out, Instruction* instr)
 {
-   char *word = nullptr;
-   std::streamsize count = 0;
    switch (instr->format)
    {
    // FIXME: casting is broken.
@@ -24,8 +22,7 @@ void emit_instruction(asm_context ctx, std::ofstream& out, Instruction* instr)
       encoding |= instr->definitionCount() ? instr->getDefinition(0).physReg().reg << 16 : 0;
       encoding |= instr->operandCount() == 2 ? instr->getOperand(1).physReg().reg << 8 : 0;
       encoding |= instr->operandCount() ? instr->getOperand(0).physReg().reg : 0;
-      word = reinterpret_cast<char*>(&encoding);
-      count = 4;
+      out.push_back(encoding);
       break;
    }
    case Format::SOPK: {
@@ -37,8 +34,7 @@ void emit_instruction(asm_context ctx, std::ofstream& out, Instruction* instr)
          instr->operandCount() && instr->getOperand(0).regClass() != RegClass::b ?
          instr->getOperand(0).physReg().reg << 16 : 0;
       encoding |= static_cast<SOPK_instruction*>(instr)->imm;
-      word = reinterpret_cast<char*>(&encoding);
-      count = 4;
+      out.push_back(encoding);
       break;
    }
    case Format::SOP1: {
@@ -46,16 +42,14 @@ void emit_instruction(asm_context ctx, std::ofstream& out, Instruction* instr)
       encoding |= instr->definitionCount() ? instr->getDefinition(0).physReg().reg << 16 : 0;
       encoding |= opcode_infos[(int)instr->opcode].opcode << 8;
       encoding |= instr->operandCount() ? instr->getOperand(0).physReg().reg : 0;
-      word = reinterpret_cast<char*>(&encoding);
-      count = 4;
+      out.push_back(encoding);
       break;
    }
    case Format::SOPP: {
       uint32_t encoding = (0b101111111 << 23);
       encoding |= opcode_infos[(int)instr->opcode].opcode << 16;
       encoding |= static_cast<SOPP_instruction*>(instr)->imm;
-      word = reinterpret_cast<char*>(&encoding);
-      count = 4;
+      out.push_back(encoding);
       break;
    }
    case Format::VOP2: {
@@ -64,8 +58,7 @@ void emit_instruction(asm_context ctx, std::ofstream& out, Instruction* instr)
       encoding |= instr->getDefinition(0).physReg().reg << 17;
       encoding |= instr->getOperand(1).physReg().reg << 9;
       encoding |= instr->getOperand(0).physReg().reg;
-      word = reinterpret_cast<char*>(&encoding);
-      count = 4;
+      out.push_back(encoding);
       break;
    }
    case Format::VOPC: {
@@ -73,8 +66,7 @@ void emit_instruction(asm_context ctx, std::ofstream& out, Instruction* instr)
       encoding |= opcode_infos[(int)instr->opcode].opcode << 17;
       encoding |= instr->getOperand(1).physReg().reg << 9;
       encoding |= instr->getOperand(0).physReg().reg;
-      word = reinterpret_cast<char*>(&encoding);
-      count = 4;
+      out.push_back(encoding);
       break;
    }
    case Format::VINTRP: {
@@ -85,39 +77,37 @@ void emit_instruction(asm_context ctx, std::ofstream& out, Instruction* instr)
       encoding |= interp->attribute << 10;
       encoding |= interp->component << 8;
       encoding |= instr->getOperand(0).physReg().reg;
-      word = reinterpret_cast<char*>(&encoding);
-      count = 4;
+      out.push_back(encoding);
       break;
    }
    case Format::EXP: {
       Export_instruction* exp = static_cast<Export_instruction*>(instr);
       uint64_t encoding = 0b110001 << 26;
-      encoding |= (uint64_t) exp->getOperand(0).physReg().reg << 32;
-      encoding |= (uint64_t) exp->getOperand(1).physReg().reg << 40;
-      encoding |= (uint64_t) exp->getOperand(2).physReg().reg << 48;
-      encoding |= (uint64_t) exp->getOperand(3).physReg().reg << 56;
       encoding |= exp->valid_mask ? 0b1 << 12 : 0;
       encoding |= exp->done ? 0b1 << 11 : 0;
       encoding |= exp->compressed ? 0b1 << 10 : 0;
       encoding |= exp->dest << 4;
       encoding |= exp->enabled_mask;
-      word = reinterpret_cast<char*>(&encoding);
-      count = 8;
+      out.push_back(encoding);
+      encoding = (uint64_t) exp->getOperand(0).physReg().reg;
+      encoding |= (uint64_t) exp->getOperand(1).physReg().reg << 8;
+      encoding |= (uint64_t) exp->getOperand(2).physReg().reg << 16;
+      encoding |= (uint64_t) exp->getOperand(3).physReg().reg << 24;
+      out.push_back(encoding);
       break;
    }
    default:
       unreachable("unimplemented instruction format");
    }
-   out.write(word, count);
    /* append literal dword */
    if (instr->operandCount() && instr->getOperand(0).physReg().reg == 255)
    {
       uint32_t literal = instr->getOperand(0).constantValue();
-      out.write(reinterpret_cast<char*>(&literal), 4);
+      out.push_back(literal);
    }
 }
 
-void emit_block(asm_context ctx, std::ofstream& out, Block* block)
+void emit_block(asm_context ctx, std::vector<uint32_t>& out, Block* block)
 {
    // TODO: emit offsets on previous branches to this block
    //std::iostream::pos_type current = out.tellp();
@@ -126,19 +116,21 @@ void emit_block(asm_context ctx, std::ofstream& out, Block* block)
       emit_instruction(ctx, out, instr.get());
 }
 
-void emit_elf_header(asm_context ctx, std::ofstream& out, Program* program)
+void emit_elf_header(asm_context ctx, std::vector<uint32_t>& out, Program* program)
 {
    // TODO
 }
 
-void emit_program(std::ofstream& out, Program* program)
+std::vector<uint32_t> emit_program(Program* program)
 {
    // TODO: initialize context
    asm_context ctx;
+   std::vector<uint32_t> out;
    emit_elf_header(ctx, out, program);
    for (auto const& block : program->blocks)
       emit_block(ctx, out, block.get());
    // footer?
+   return out;
 }
 
 }
