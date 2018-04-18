affeaa6639203aae7803faf86c6bf3d5ed19d3c2
Iago Toral Quiroga <itoral@igalia.com>
2018-04-18 10:50:35 +0200

    compiler/nir: add lowering option for 16-bit fmod
    Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>

--- src/compiler/nir/nir.h
+++ src/compiler/nir/nir.h
@@ -2080,6 +2080,7 @@ typedef struct nir_shader_compiler_options {
    bool lower_fpow;
    bool lower_fsat;
    bool lower_fsqrt;
+   bool lower_fmod16;
    bool lower_fmod32;
    bool lower_fmod64;
    /** Lowers ibitfield_extract/ubitfield_extract to ibfe/ubfe. */
