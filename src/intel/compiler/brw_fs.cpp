08eae467eeb58e4f66a36b88869df40164fd9415
Samuel Iglesias Gonsálvez <siglesias@igalia.com>
2018-06-06 15:09:55 +0200

    i965/fs: emit shader float controls execution modes as first instruction of shaders
    Signed-off-by: Samuel Iglesias Gonsálvez <siglesias@igalia.com>

--- src/intel/compiler/brw_fs.cpp
+++ src/intel/compiler/brw_fs.cpp
@@ -7239,6 +7239,8 @@ fs_visitor::run_vs()
    if (shader_time_index >= 0)
       emit_shader_time_begin();
 
+   emit_shader_float_controls_execution_mode();
+
    emit_nir_code();
 
    if (failed)
@@ -7308,6 +7310,7 @@ fs_visitor::run_tcs_single_patch()
               brw_imm_ud(nir->info.tess.tcs_vertices_out), BRW_CONDITIONAL_L);
       bld.IF(BRW_PREDICATE_NORMAL);
    }
+   emit_shader_float_controls_execution_mode();
 
    emit_nir_code();
 
@@ -7359,6 +7362,8 @@ fs_visitor::run_tes()
    if (shader_time_index >= 0)
       emit_shader_time_begin();
 
+   emit_shader_float_controls_execution_mode();
+
    emit_nir_code();
 
    if (failed)
@@ -7407,6 +7412,8 @@ fs_visitor::run_gs()
 
    if (shader_time_index >= 0)
       emit_shader_time_begin();
+
+   emit_shader_float_controls_execution_mode();
 
    emit_nir_code();
 
@@ -7498,6 +7505,8 @@ fs_visitor::run_fs(bool allow_spilling, bool do_rep_send)
                  retype(dispatch_mask, BRW_REGISTER_TYPE_UW));
       }
 
+      emit_shader_float_controls_execution_mode();
+
       emit_nir_code();
 
       if (failed)
@@ -7552,6 +7561,8 @@ fs_visitor::run_cs(unsigned min_dispatch_width)
       abld.MOV(retype(brw_sr0_reg(1), BRW_REGISTER_TYPE_UW),
                suboffset(retype(brw_vec1_grf(0, 0), BRW_REGISTER_TYPE_UW), 1));
    }
+
+   emit_shader_float_controls_execution_mode();
 
    emit_nir_code();
 
