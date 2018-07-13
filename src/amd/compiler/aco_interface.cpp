4ffcf9b029e09528e0de3e580480a2a99a626bbe
Daniel Schürmann <daniel.schuermann@campus.tu-berlin.de>
2018-07-13 13:32:20 +0200

    amd/compiler: enable control flow (without optimization pass)

--- src/amd/compiler/aco_interface.cpp
+++ src/amd/compiler/aco_interface.cpp
@@ -46,10 +46,7 @@ void aco_compile_shader(struct nir_shader *shader, struct ac_shader_config* conf
       options = &local_options;
    }
 
-   /* return if shader contains multiple blocks from control flow */
    struct nir_function *func = (struct nir_function *)exec_list_get_head(&shader->functions);
-   if (func->impl->num_blocks > 1)
-      return;
 
    struct radv_shader_info pre_info = info->info;
 
@@ -66,8 +63,9 @@ void aco_compile_shader(struct nir_shader *shader, struct ac_shader_config* conf
    aco_print_program(program.get(), stderr);
    aco::validate(program.get(), stderr);
 
-   /* Optimization */
-   aco::optimize(program.get());
+   /* Optimization: doesn't work yet with control flow */
+   if (func->impl->num_blocks == 1)
+      aco::optimize(program.get());
    std::cerr << "After Opt:\n";
    aco_print_program(program.get(), stderr);
    aco::validate(program.get(), stderr);
