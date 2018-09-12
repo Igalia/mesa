Igalia/mesa

commit 066f44339a41b6dada38bb1be318a06e355cb0a9

main/shader_query: Fix resource name query if no names
Do not add '[0]' to the name of an uniform array if the
resource basename is NULL.

src/mesa/main/shader_query.cpp
@@ -826,7 +826,11 @@ _mesa_get_program_resource_name(struct gl_shader_program *shProg,
 
    _mesa_copy_string(name, bufSize, length, _mesa_program_resource_name(res));
 
-   if (_mesa_program_resource_array_size(res) && add_index_to_name(res)) {
+   /* The resource name can be NULL for shaders constructed from SPIR-V
+    * binaries. In that case, we do not add the '[0]'.
+    */
+   if (name[0] != '\0' &&
+       _mesa_program_resource_array_size(res) && add_index_to_name(res)) {
       int i;
 
       /* The comparison is strange because *length does *NOT* include the

commit 92d91a091011e9e9eb6238aa6379615e80800486

main/shader_query: Fix resource name query if no names 2
Fixes: 066f44339a4 ("main/shader_query: Fix resource name query if no names")
Signed-off-by: Andres Gomez <agomez@igalia.com>

src/mesa/main/shader_query.cpp
@@ -829,7 +829,7 @@ _mesa_get_program_resource_name(struct gl_shader_program *shProg,
    /* The resource name can be NULL for shaders constructed from SPIR-V
     * binaries. In that case, we do not add the '[0]'.
     */
-   if (name[0] != '\0' &&
+   if (name && name[0] != '\0' &&
        _mesa_program_resource_array_size(res) && add_index_to_name(res)) {
       int i;
 