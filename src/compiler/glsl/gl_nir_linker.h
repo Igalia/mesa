7b01d5bf236782707cdc36f0bf9e78be99704714
Antia Puentes <apuentes@igalia.com>
2018-09-12 10:47:35 +0200

    nir/linker: Minor refactor
    (cherry picked from commit 16b6d86017e5463653dfe68cf62e7f2fb2a72244)

--- src/compiler/glsl/gl_nir_linker.h
+++ src/compiler/glsl/gl_nir_linker.h
@@ -49,6 +49,8 @@ void gl_nir_link_assign_xfb_resources(struct gl_context *ctx,
 bool gl_nir_link_uniform_blocks(struct gl_context *ctx,
                                 struct gl_shader_program *prog);
 
+bool _glsl_type_is_leaf(const struct glsl_type *type);
+
 #ifdef __cplusplus
 } /* extern "C" */
 #endif
