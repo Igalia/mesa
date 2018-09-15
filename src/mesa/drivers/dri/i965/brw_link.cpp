ba21b98d33e4a4edfed2f9936cb636029b7af186
Alejandro Piñeiro <apinheiro@igalia.com>
2018-09-15 16:42:58 +0200

    i965: use GLboolean for all brw_link_shader returns
    The function had a mix of true/GL_TRUE and false/GL_FALSE
    returns. Using GL_TRUE/GL_FALSE as the function returns a GLboolean.
    
    (cherry picked from commit d4693b700fb76de6c6b96a9a0b2d4b26eeae65e6)

--- src/mesa/drivers/dri/i965/brw_link.cpp
+++ src/mesa/drivers/dri/i965/brw_link.cpp
@@ -264,7 +264,7 @@ brw_link_shader(struct gl_context *ctx, struct gl_shader_program *shProg)
    /* SPIR-V programs use a NIR linker */
    if (shProg->data->spirv) {
       if (!gl_nir_link_uniforms(ctx, shProg))
-         return false;
+         return GL_FALSE;
 
       gl_nir_link_assign_atomic_counter_resources(ctx, shProg);
       gl_nir_link_assign_xfb_resources(ctx, shProg);
@@ -377,7 +377,7 @@ brw_link_shader(struct gl_context *ctx, struct gl_shader_program *shProg)
    }
 
    if (brw->precompile && !brw_shader_precompile(ctx, shProg))
-      return false;
+      return GL_FALSE;
 
    /* SPIR-V programs build its resource list from linked NIR shaders. */
    if (!shProg->data->spirv)
@@ -395,5 +395,5 @@ brw_link_shader(struct gl_context *ctx, struct gl_shader_program *shProg)
       shader->ir = NULL;
    }
 
-   return true;
+   return GL_TRUE;
 }
