c652d9176ed9f08118a84c4b166b0705442d09f2
Samuel Iglesias Gonsálvez <siglesias@igalia.com>
2018-11-19 12:38:10 +0100

    i965/fs: add support for shader float control to remove_extra_rounding_modes()
    The remove_extra_rounding_modes() optimization will remove duplicated
    rounding mode changes.
    
    v2:
    - Fix bug in the rounding mode change (Alejandro)
    
    v3:
    - Fix rounding modes.
    
    Signed-off-by: Samuel Iglesias Gonsálvez <siglesias@igalia.com>

--- src/intel/compiler/brw_fs.cpp
+++ src/intel/compiler/brw_fs.cpp
@@ -3486,9 +3486,20 @@ bool
 fs_visitor::remove_extra_rounding_modes()
 {
    bool progress = false;
+   unsigned execution_mode = this->nir->info.shader_float_controls_execution_mode;
+
+   brw_rnd_mode base_mode = BRW_RND_MODE_UNSPECIFIED;
+   if ((execution_mode & SHADER_ROUNDING_MODE_RTE_FP16) ||
+       (execution_mode & SHADER_ROUNDING_MODE_RTE_FP32) ||
+       (execution_mode & SHADER_ROUNDING_MODE_RTE_FP64))
+      base_mode = BRW_RND_MODE_RTNE;
+   if ((execution_mode & SHADER_ROUNDING_MODE_RTZ_FP16) ||
+       (execution_mode & SHADER_ROUNDING_MODE_RTZ_FP32) ||
+       (execution_mode & SHADER_ROUNDING_MODE_RTZ_FP64))
+      base_mode = BRW_RND_MODE_RTZ;
 
    foreach_block (block, cfg) {
-      brw_rnd_mode prev_mode = BRW_RND_MODE_UNSPECIFIED;
+      brw_rnd_mode prev_mode = base_mode;
 
       foreach_inst_in_block_safe (fs_inst, inst, block) {
          if (inst->opcode == SHADER_OPCODE_RND_MODE) {
