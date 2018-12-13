194ad2f263f01b6c976cd0abd5c12c406d591f1d
Alejandro Piñeiro <apinheiro@igalia.com>
2018-12-13 16:40:35 +0100

    spirv/nir: only set nir interface_type for block input/outputs
    vnt_variables uses interface_type on several use cases, but on nir
    variable it is more limited, and tied to be an interface block. From
    nir.h:
    
       /**
        * For variables that are in an interface block or are an instance of an
        * interface block, this is the \c GLSL_TYPE_INTERFACE type for that block.
        *
        * \sa ir_variable::location
        */
    
    But interface blocks expects the type to be an struct, and at the same
    time, we need to differentiate just a struct from a interface
    block. Although in most cases they behave the same, it is not always
    the case (like xfb_offset/xfb_buffer assignment on arrays of structs
    vs arrays of blocks), so we need to differentiate a struct from a
    block, and specially an array of structs from an array of blocks.
    
    The latter needs to be use-case handled based on the stage.
    
    (cherry picked from commit 9ef830c900641136470e285e36e143679eed6345)

--- src/compiler/spirv/vtn_variables.c
+++ src/compiler/spirv/vtn_variables.c
@@ -1684,6 +1684,27 @@ is_per_vertex_inout(const struct vtn_variable *var, gl_shader_stage stage)
    return false;
 }
 
+static bool
+is_user_defined_block_array(const struct vtn_variable *var, gl_shader_stage stage)
+{
+   if (!glsl_type_is_array(var->type->type))
+      return false;
+
+   if (!var->type->array_element->block)
+      return false;
+
+   if (var->mode == vtn_variable_mode_input) {
+      return stage == MESA_SHADER_FRAGMENT;
+   }
+
+   if (var->mode == vtn_variable_mode_output) {
+      return stage == MESA_SHADER_VERTEX;
+   }
+
+   return false;
+}
+
+
 static void
 vtn_create_variable(struct vtn_builder *b, struct vtn_value *val,
                     struct vtn_type *ptr_type, SpvStorageClass storage_class,
@@ -1808,12 +1829,26 @@ vtn_create_variable(struct vtn_builder *b, struct vtn_value *val,
          interface_type = var->type->array_element;
       }
 
+      if (is_user_defined_block_array(var, b->shader->info.stage)) {
+         /* For Vertex shader outputs, and fragment shader inputs, block
+          * arrays need to be splitted, in order to support interpolator
+          * qualifiers for that case, and some xfb cases. For other similar
+          * cases, like an array of structs, we leave the original type, as
+          * the split is not needed, and in fact is more easy to handle the
+          * full type (like on xfb gathering info)
+          */
+         interface_type = var->type->array_element;
+      }
+
       var->var = rzalloc(b->shader, nir_variable);
       var->var->name = ralloc_strdup(var->var, val->name);
       var->var->type = var->type->type;
-      var->var->interface_type = interface_type->type;
       var->var->data.mode = nir_mode;
       var->var->data.patch = var->patch;
+
+      if (interface_type->block) {
+         var->var->interface_type = interface_type->type;
+      }
 
       if (glsl_type_is_struct(interface_type->type)) {
          /* It's a struct.  Set it up as per-member. */
