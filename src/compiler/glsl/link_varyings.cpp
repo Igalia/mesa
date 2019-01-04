yuq/mesa-lima

commit 13652e7516a3ef75c5ae75ffa5c7f048629bf772

glsl/linker: Fix type checks for location aliasing
From the OpenGL 4.6 spec, section 4.4.1 Input Layout Qualifiers, Page 68,
(Location aliasing):

   "Further, when location aliasing, the aliases sharing the location
    must have the same underlying numerical type  (floating-point or
    integer)."

The current implementation is too strict, since it checks that the
the base types are an exact match instead.

Reviewed-by: Ilia Mirkin <imirkin@alum.mit.edu>

src/compiler/glsl/link_varyings.cpp
@@ -405,12 +405,27 @@ compute_variable_location_slot(ir_variable *var, gl_shader_stage stage)
 
 struct explicit_location_info {
    ir_variable *var;
-   unsigned base_type;
+   unsigned numerical_type;
    unsigned interpolation;
    bool centroid;
    bool sample;
    bool patch;
 };
+
+static inline unsigned
+get_numerical_type(const glsl_type *type)
+{
+   /* From the OpenGL 4.6 spec, section 4.4.1 Input Layout Qualifiers, Page 68,
+    * (Location aliasing):
+    *
+    *    "Further, when location aliasing, the aliases sharing the location
+    *     must have the same underlying numerical type  (floating-point or
+    *     integer)
+    */
+   if (type->is_float() || type->is_double())
+      return GLSL_TYPE_FLOAT;
+   return GLSL_TYPE_INT;
+}
 
 static bool
 check_location_aliasing(struct explicit_location_info explicit_locations[][4],
@@ -456,7 +471,8 @@ check_location_aliasing(struct explicit_location_info explicit_locations[][4],
                /* For all other used components we need to have matching
                 * types, interpolation and auxiliary storage
                 */
-               if (info->base_type != type->without_array()->base_type) {
+               if (info->numerical_type !=
+                   get_numerical_type(type->without_array())) {
                   linker_error(prog,
                                "Varyings sharing the same location must "
                                "have the same underlying numerical type. "
@@ -486,7 +502,7 @@ check_location_aliasing(struct explicit_location_info explicit_locations[][4],
             }
          } else if (comp >= component && comp < last_comp) {
             info->var = var;
-            info->base_type = type->without_array()->base_type;
+            info->numerical_type = get_numerical_type(type->without_array());
             info->interpolation = interpolation;
             info->centroid = centroid;
             info->sample = sample;

Igalia/mesa

commit 2bd16914966b875751c3111890f25dd4d07fe17a

glsl/linker: Mind type width for location aliasing checks
From page 67 (page 71 of the PDF) of the GLSL 4.60 v.5 spec:

  " Further, when location aliasing, the aliases sharing the location
    must have the same underlying numerical type and bit
    width (floating-point or integer, 32-bit versus 64-bit, etc.)"

The current implementation didn't take the width of the type into account.

Fixes: 13652e7516a ("glsl/linker: Fix type checks for location aliasing")
Cc: Iago Toral Quiroga <itoral@igalia.com>
Signed-off-by: Andres Gomez <agomez@igalia.com>

src/compiler/glsl/link_varyings.cpp
@@ -424,7 +424,7 @@ compute_variable_location_slot(ir_variable *var, gl_shader_stage stage)
 
 struct explicit_location_info {
    ir_variable *var;
-   unsigned numerical_type;
+   unsigned aliasing_type;
    unsigned interpolation;
    bool centroid;
    bool sample;
@@ -432,17 +432,20 @@ struct explicit_location_info {
 };
 
 static inline unsigned
-get_numerical_type(const glsl_type *type)
+get_aliasing_type(const glsl_type *type)
 {
-   /* From the OpenGL 4.6 spec, section 4.4.1 Input Layout Qualifiers, Page 68,
-    * (Location aliasing):
+   /* From the OpenGL 4.6 v.5 spec, section 4.4.1 Input Layout Qualifiers,
+    * Page 67, (Location aliasing):
     *
     *    "Further, when location aliasing, the aliases sharing the location
-    *     must have the same underlying numerical type  (floating-point or
-    *     integer)
+    *     must have the same underlying numerical type and bit width
+    *     (floating-point or integer, 32-bit versus 64-bit, etc.)"
+    *
     */
-   if (type->is_float() || type->is_double())
+   if (type->is_float())
       return GLSL_TYPE_FLOAT;
+   if (type->is_double())
+      return GLSL_TYPE_DOUBLE;
    return GLSL_TYPE_INT;
 }
 
@@ -491,12 +494,12 @@ check_location_aliasing(struct explicit_location_info explicit_locations[][4],
                /* For all other used components we need to have matching
                 * types, interpolation and auxiliary storage
                 */
-               if (info->numerical_type !=
-                   get_numerical_type(type->without_array())) {
+               if (info->aliasing_type !=
+                   get_aliasing_type(type->without_array())) {
                   linker_error(prog,
                                "Varyings sharing the same location must "
-                               "have the same underlying numerical type. "
-                               "Location %u component %u\n",
+                               "have the same underlying numerical type and "
+                               "bit width. Location %u component %u\n",
                                location, comp);
                   return false;
                }