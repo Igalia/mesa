29dd6d02393fe0edb9543055bc12a57da54318d5
Alejandro Piñeiro <apinheiro@igalia.com>
2019-01-14 12:42:31 +0100

    nir/linker: refactor vulkan intrinsics lowerings
    As in both cases is basically a callback that is called while
    iterating through intrinsics.
    
    v2: as we are refactoring, give more meaningful names to method
        convert_impl (->walk_intrinsics) and parameter callback
        (->lower_intrinsic) (Timothy Arceri)
    
    Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>
    (cherry picked from commit 0bd5692e697e8fdc6283925e3446125aa81b1332)

--- src/compiler/glsl/gl_nir_lower_vulkan_intrinsics.c
+++ src/compiler/glsl/gl_nir_lower_vulkan_intrinsics.c
@@ -35,6 +35,9 @@
  * is intended to be used with GL_ARB_gl_spirv. Unlike Vulkan, it is not
  * necessary to wait for the complete pipeline state to lower it.
  */
+typedef bool (*nir_lower_vulkan_intrinsic_cb) (struct nir_builder *b,
+                                               nir_intrinsic_instr *intrin,
+                                               struct gl_linked_shader *linked_shader);
 
 static unsigned
 find_block_by_binding(unsigned num_blocks,
@@ -79,68 +82,66 @@ find_intrinsic_usage(nir_ssa_def *def,
 }
 
 static bool
-convert_block(nir_block *block,
-              struct gl_linked_shader *linked_shader,
-              nir_builder *b)
+lower_vulkan_resource_index(struct nir_builder *b,
+                            nir_intrinsic_instr *intrin,
+                            struct gl_linked_shader *linked_shader)
 {
-   bool progress = false;
+   bool is_ubo_usage;
+   if (!find_intrinsic_usage(&intrin->dest.ssa, &is_ubo_usage))
+      return false;
 
-   nir_foreach_instr_safe(instr, block) {
-      if (instr->type != nir_instr_type_intrinsic)
-         continue;
+   b->cursor = nir_after_instr(&intrin->instr);
 
-      nir_intrinsic_instr *res_index = nir_instr_as_intrinsic(instr);
+   /* The descriptor set should always be zero for GL */
+   assert(nir_intrinsic_desc_set(intrin) == 0);
+   unsigned binding = nir_intrinsic_binding(intrin);
 
-      if (res_index->intrinsic != nir_intrinsic_vulkan_resource_index)
-         continue;
+   unsigned num_blocks;
+   struct gl_uniform_block **blocks;
 
-      bool is_ubo_usage;
-      if (!find_intrinsic_usage(&res_index->dest.ssa, &is_ubo_usage))
-         continue;
-
-      b->cursor = nir_after_instr(instr);
-
-      /* The descriptor set should always be zero for GL */
-      assert(nir_intrinsic_desc_set(res_index) == 0);
-      unsigned binding = nir_intrinsic_binding(res_index);
-
-      unsigned num_blocks;
-      struct gl_uniform_block **blocks;
-
-      if (is_ubo_usage) {
-         num_blocks = linked_shader->Program->info.num_ubos;
-         blocks = linked_shader->Program->sh.UniformBlocks;
-      } else {
-         num_blocks = linked_shader->Program->info.num_ssbos;
-         blocks = linked_shader->Program->sh.ShaderStorageBlocks;
-      }
-
-      unsigned block = find_block_by_binding(num_blocks, blocks, binding);
-
-      nir_ssa_def *surface =
-         nir_iadd(b,
-                  nir_imm_int(b, block),
-                  nir_ssa_for_src(b, res_index->src[0], 1));
-
-      nir_ssa_def_rewrite_uses(&res_index->dest.ssa, nir_src_for_ssa(surface));
-      nir_instr_remove(instr);
-
-      progress = true;
+   if (is_ubo_usage) {
+      num_blocks = linked_shader->Program->info.num_ubos;
+      blocks = linked_shader->Program->sh.UniformBlocks;
+   } else {
+      num_blocks = linked_shader->Program->info.num_ssbos;
+      blocks = linked_shader->Program->sh.ShaderStorageBlocks;
    }
 
-   return progress;
+   unsigned block = find_block_by_binding(num_blocks, blocks, binding);
+
+   nir_ssa_def *surface =
+      nir_iadd(b,
+               nir_imm_int(b, block),
+               nir_ssa_for_src(b, intrin->src[0], 1));
+
+   nir_ssa_def_rewrite_uses(&intrin->dest.ssa, nir_src_for_ssa(surface));
+   nir_instr_remove(&intrin->instr);
+
+   return true;
 }
 
 static bool
-convert_impl(nir_function_impl *impl,
-             struct gl_linked_shader *linked_shader)
+walk_intrinsics(nir_function_impl *impl,
+                nir_lower_vulkan_intrinsic_cb lower_intrinsic,
+                nir_intrinsic_op intrin_op,
+                struct gl_linked_shader *linked_shader)
 {
    bool progress = false;
    nir_builder builder;
    nir_builder_init(&builder, impl);
 
    nir_foreach_block(block, impl) {
-      progress |= convert_block(block, linked_shader, &builder);
+      nir_foreach_instr_safe(instr, block) {
+         if (instr->type != nir_instr_type_intrinsic)
+            continue;
+
+         nir_intrinsic_instr *intrin = nir_instr_as_intrinsic(instr);
+
+         if (intrin->intrinsic != intrin_op)
+            continue;
+
+         progress |= lower_intrinsic(&builder, intrin, linked_shader);
+      }
    }
 
    nir_metadata_preserve(impl, nir_metadata_block_index |
@@ -149,8 +150,9 @@ convert_impl(nir_function_impl *impl,
 }
 
 static bool
-lower_load_vulkan_descriptor(nir_intrinsic_instr *intrin,
-                             nir_builder *b)
+lower_load_vulkan_descriptor(nir_builder *b,
+                             nir_intrinsic_instr *intrin,
+                             struct gl_linked_shader *linked_shader)
 {
    b->cursor = nir_before_instr(&intrin->instr);
 
@@ -165,32 +167,6 @@ lower_load_vulkan_descriptor(nir_intrinsic_instr *intrin,
    return true;
 }
 
-static bool
-lower_vulkan_descriptor_impl(nir_function_impl *impl,
-                             struct gl_linked_shader *linked_shader)
-{
-   bool progress = false;
-   nir_builder builder;
-   nir_builder_init(&builder, impl);
-
-   nir_foreach_block(block, impl) {
-      nir_foreach_instr_safe(instr, block) {
-         if (instr->type != nir_instr_type_intrinsic)
-            continue;
-
-         nir_intrinsic_instr *intrin = nir_instr_as_intrinsic(instr);
-         if (intrin->intrinsic != nir_intrinsic_load_vulkan_descriptor)
-            continue;
-
-         progress |= lower_load_vulkan_descriptor(intrin, &builder);
-      }
-   }
-
-   nir_metadata_preserve(impl, nir_metadata_block_index |
-                               nir_metadata_dominance);
-   return progress;
-}
-
 bool
 gl_nir_lower_vulkan_descriptor(nir_shader *shader,
                                struct gl_linked_shader *linked_shader)
@@ -199,7 +175,8 @@ gl_nir_lower_vulkan_descriptor(nir_shader *shader,
 
    nir_foreach_function(function, shader) {
       if (function->impl)
-         progress = lower_vulkan_descriptor_impl(function->impl, linked_shader) || progress;
+         progress = walk_intrinsics(function->impl, lower_load_vulkan_descriptor,
+                                    nir_intrinsic_load_vulkan_descriptor, linked_shader) || progress;
    }
 
    return progress;
@@ -214,7 +191,8 @@ gl_nir_lower_vulkan_resource_index(nir_shader *shader,
 
    nir_foreach_function(function, shader) {
       if (function->impl)
-         progress = convert_impl(function->impl, linked_shader) || progress;
+         progress = walk_intrinsics(function->impl, lower_vulkan_resource_index,
+                                    nir_intrinsic_vulkan_resource_index, linked_shader) || progress;
    }
 
    return progress;
