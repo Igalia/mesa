b9e1e3ad4bd69f7d0fa60cd5d55012bf048e8d55
Iago Toral Quiroga <itoral@igalia.com>
2019-01-22 11:26:03 +0100

    compiler/spirv: add support for Float16 capability
    v2:
      - Merge Float16 and Int8 capabilities into a single patch (Jason)
    
    v3:
      - Dropped Int8 capability since it was already merged by another
        patch.
    
    Reviewed-by: Jason Ekstrand <jason@jlekstrand.net> (v1)

--- src/compiler/spirv/spirv_to_nir.c
+++ src/compiler/spirv/spirv_to_nir.c
@@ -3561,7 +3561,6 @@ vtn_handle_preamble_instruction(struct vtn_builder *b, SpvOp opcode,
       case SpvCapabilityLinkage:
       case SpvCapabilityVector16:
       case SpvCapabilityFloat16Buffer:
-      case SpvCapabilityFloat16:
       case SpvCapabilitySparseResidency:
          vtn_warn("Unsupported SPIR-V capability: %s",
                   spirv_capability_to_string(cap));
@@ -3577,6 +3576,9 @@ vtn_handle_preamble_instruction(struct vtn_builder *b, SpvOp opcode,
 
       case SpvCapabilityFloat64:
          spv_check_supported(float64, cap);
+         break;
+      case SpvCapabilityFloat16:
+         spv_check_supported(float16, cap);
          break;
       case SpvCapabilityInt64:
          spv_check_supported(int64, cap);
