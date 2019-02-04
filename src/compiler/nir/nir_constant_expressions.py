15f5a0fede8e6d10ed51fb439ccba478b3390734
Samuel Iglesias Gonsálvez <siglesias@igalia.com>
2019-02-04 15:10:35 +0100

    nir/constant_expressions: take into account rounding mode to convert from float to float16 destinations
    Signed-off-by: Samuel Iglesias Gonsálvez <siglesias@igalia.com>

--- src/compiler/nir/nir_constant_expressions.py
+++ src/compiler/nir/nir_constant_expressions.py
@@ -371,7 +371,17 @@ struct ${type}${width}_vec {
             ## Sanitize the C value to a proper NIR 0/-1 bool
             _dst_val[_i].${get_const_field(output_type)} = -(int)dst;
          % elif output_type == "float16":
-            _dst_val[_i].u16 = _mesa_float_to_half(dst);
+            % if "rtz" in op.name:
+               _dst_val[_i].u16 = _mesa_float_to_float16_rtz(dst);
+            % elif "rtne" in op.name:
+               _dst_val[_i].u16 = _mesa_float_to_float16_rtne(dst);
+            % else:
+               if (nir_is_rounding_mode_rtz(execution_mode, 16)) {
+                  _dst_val[_i].u16 = _mesa_float_to_float16_rtz(dst);
+               } else {
+                  _dst_val[_i].u16 = _mesa_float_to_float16_rtne(dst);
+               }
+            %endif
          % else:
             _dst_val[_i].${get_const_field(output_type)} = dst;
          % endif
@@ -415,7 +425,17 @@ struct ${type}${width}_vec {
             ## Sanitize the C value to a proper NIR 0/-1 bool
             _dst_val[${k}].${get_const_field(output_type)} = -(int)dst.${"xyzw"[k]};
          % elif output_type == "float16":
-            _dst_val[${k}].u16 = _mesa_float_to_half(dst.${"xyzw"[k]});
+            % if "rtz" in op.name:
+               _dst_val[${k}].u16 = _mesa_float_to_float16_rtz(dst.${"xyzw"[k]});
+            % elif "rtne" in op.name:
+               _dst_val[${k}].u16 = _mesa_float_to_float16_rtne(dst.${"xyzw"[k]});
+            % else:
+               if (nir_is_rounding_mode_rtz(execution_mode, 16)) {
+                  _dst_val[${k}].u16 = _mesa_float_to_float16_rtz(dst.${"xyzw"[k]});
+               } else {
+                  _dst_val[${k}].u16 = _mesa_float_to_float16_rtne(dst.${"xyzw"[k]});
+               }
+            %endif
          % else:
             _dst_val[${k}].${get_const_field(output_type)} = dst.${"xyzw"[k]};
          % endif
