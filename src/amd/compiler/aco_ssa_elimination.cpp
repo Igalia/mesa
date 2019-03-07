f3ca80ad75a6d2aa910d9160faaf825efb0d7726
Daniel Schürmann <daniel.schuermann@campus.tu-berlin.de>
2019-03-07 10:57:28 +0100

    amd/compiler: refactor SSA elimination

--- src/amd/compiler/aco_ssa_elimination.cpp
+++ src/amd/compiler/aco_ssa_elimination.cpp
@@ -20,9 +20,6 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  *
- * Authors:
- *    Daniel Schürmann (daniel.schuermann@campus.tu-berlin.de)
- *
  */
 
 
@@ -31,78 +28,94 @@
 #include <map>
 
 namespace aco {
+namespace {
 
-/* we use a map: block-id -> pair (dest, src) to store phi information */
+/* map: block-id -> pair (dest, src) to store phi information */
 typedef std::map<uint32_t, std::vector<std::pair<Definition, Operand>>> phi_info;
 
 struct ssa_elimination_ctx {
    phi_info logical_phi_info;
    phi_info linear_phi_info;
+   Program* program;
+
+   ssa_elimination_ctx(Program* program) : program(program) {}
 };
 
-void collect_phi_info(phi_info& ctx, aco_ptr<Instruction>& phi, std::vector<Block*> preds)
+void collect_phi_info(ssa_elimination_ctx& ctx)
 {
-   for (unsigned i = 0; i < preds.size(); i++)
-   {
-      if (phi->getOperand(i).isUndefined())
-         continue;
-      const auto result = ctx.emplace(preds[i]->index, std::vector<std::pair<Definition, Operand>>());
-      result.first->second.emplace_back(phi->getDefinition(0), phi->getOperand(i));
-   }
+   for (std::unique_ptr<Block>& block : ctx.program->blocks) {
+      for (aco_ptr<Instruction>& phi : block->instructions) {
+         if (phi->opcode == aco_opcode::p_phi || phi->opcode == aco_opcode::p_linear_phi) {
+            for (unsigned i = 0; i < phi->num_operands; i++) {
+               if (phi->getOperand(i).isUndefined())
+                  continue;
+               if (phi->getOperand(i).isTemp() && phi->getOperand(i).physReg() == phi->getDefinition(0).physReg())
+                  continue;
 
-}
-
-void eliminate_phis(Program* program)
-{
-   ssa_elimination_ctx ctx;
-
-   /* 1. Collect information about every phi-instruction */
-   for (auto&& block : program->blocks) {
-      for (aco_ptr<Instruction>& instr : block->instructions)
-      {
-         if (instr->opcode == aco_opcode::p_phi)
-            collect_phi_info(ctx.logical_phi_info, instr, block->logical_predecessors);
-         if (instr->opcode == aco_opcode::p_linear_phi)
-            collect_phi_info(ctx.linear_phi_info, instr, block->linear_predecessors);
-      }
-   }
-
-   /* 2. we replace the p_logical_end instructions with a parallelcopy (we don't need the former anymore) */
-   for (auto&& entry : ctx.logical_phi_info) {
-      std::unique_ptr<Block>& block = program->blocks[entry.first];
-      for (std::vector<aco_ptr<Instruction>>::reverse_iterator it = block->instructions.rbegin(); ; ++it)
-      {
-         assert(it != block->instructions.rend() && "Couldn't find a p_logical_end instruction");
-         if ((*it)->opcode == aco_opcode::p_logical_end) {
-            aco_ptr<Instruction> pc{create_instruction<Instruction>(aco_opcode::p_parallelcopy, Format::PSEUDO, entry.second.size(), entry.second.size())};
-            unsigned idx = 0;
-            for (std::pair<Definition, Operand>& pair : entry.second)
-            {
-               pc->getDefinition(idx) = pair.first;
-               pc->getOperand(idx) = pair.second;
-               idx++;
+               std::vector<Block*>& preds = phi->opcode == aco_opcode::p_phi ? block->logical_predecessors : block->linear_predecessors;
+               phi_info& info = phi->opcode == aco_opcode::p_phi ? ctx.logical_phi_info : ctx.linear_phi_info;
+               const auto result = info.emplace(preds[i]->index, std::vector<std::pair<Definition, Operand>>());
+               result.first->second.emplace_back(phi->getDefinition(0), phi->getOperand(i));
             }
-            (*it).swap(pc);
+         } else {
             break;
          }
       }
    }
+}
 
-   /* 3. we insert parallelcopys for the linear phis at the end of blocks just before the branch */
+void insert_parallelcopies(ssa_elimination_ctx& ctx)
+{
+   /* insert the parallelcopies from logical phis before p_logical_end */
+   for (auto&& entry : ctx.logical_phi_info) {
+      std::unique_ptr<Block>& block = ctx.program->blocks[entry.first];
+      unsigned idx = block->instructions.size() - 1;
+      while (block->instructions[idx]->opcode != aco_opcode::p_logical_end) {
+         assert(idx > 0);
+         idx--;
+      }
+
+      std::vector<aco_ptr<Instruction>>::iterator it = std::next(block->instructions.begin(), idx);
+      aco_ptr<Instruction> pc{create_instruction<Instruction>(aco_opcode::p_parallelcopy, Format::PSEUDO, entry.second.size(), entry.second.size())};
+      unsigned i = 0;
+      for (std::pair<Definition, Operand>& pair : entry.second)
+      {
+         pc->getDefinition(i) = pair.first;
+         pc->getOperand(i) = pair.second;
+         i++;
+      }
+      block->instructions.insert(it, std::move(pc));
+   }
+
+   /* insert parallelcopies for the linear phis at the end of blocks just before the branch */
    for (auto&& entry : ctx.linear_phi_info) {
-      std::unique_ptr<Block>& block = program->blocks[entry.first];
+      std::unique_ptr<Block>& block = ctx.program->blocks[entry.first];
       std::vector<aco_ptr<Instruction>>::iterator it = block->instructions.end();
       --it;
       assert((*it)->format == Format::PSEUDO_BRANCH);
       aco_ptr<Instruction> pc{create_instruction<Instruction>(aco_opcode::p_parallelcopy, Format::PSEUDO, entry.second.size(), entry.second.size())};
-      unsigned idx = 0;
+      unsigned i = 0;
       for (std::pair<Definition, Operand>& pair : entry.second)
       {
-         pc->getDefinition(idx) = pair.first;
-         pc->getOperand(idx) = pair.second;
-         idx++;
+         pc->getDefinition(i) = pair.first;
+         pc->getOperand(i) = pair.second;
+         i++;
       }
       block->instructions.insert(it, std::move(pc));
    }
+}
+
+} /* end namespace */
+
+
+void eliminate_phis(Program* program)
+{
+   ssa_elimination_ctx ctx(program);
+
+   /* Collect information about every phi-instruction */
+   collect_phi_info(ctx);
+
+   insert_parallelcopies(ctx);
+
 }
 }
