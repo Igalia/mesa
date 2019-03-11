f310ee154542c7e7acc563bba2c893f5ea3d2574
Eleni Maria Stea <estea@igalia.com>
2019-03-11 13:33:16 +0200

    anv: Enabled the VK_EXT_sample_locations extension
    Enabled the VK_EXT_sample_locations for Intel Gen >= 7.
    
    v2: Replaced device.info->gen >= 7 with True, as Anv doesn't support
        anything below Gen7. (Lionel Landwerlin)
    
    Reviewed-by: Sagar Ghuge <sagar.ghuge@intel.com>

--- src/intel/vulkan/anv_extensions.py
+++ src/intel/vulkan/anv_extensions.py
@@ -129,7 +129,7 @@ EXTENSIONS = [
     Extension('VK_EXT_inline_uniform_block',              1, True),
     Extension('VK_EXT_pci_bus_info',                      2, True),
     Extension('VK_EXT_post_depth_coverage',               1, 'device->info.gen >= 9'),
-    Extension('VK_EXT_sample_locations',                  1, False),
+    Extension('VK_EXT_sample_locations',                  1, True),
     Extension('VK_EXT_sampler_filter_minmax',             1, 'device->info.gen >= 9'),
     Extension('VK_EXT_scalar_block_layout',               1, True),
     Extension('VK_EXT_shader_stencil_export',             1, 'device->info.gen >= 9'),
