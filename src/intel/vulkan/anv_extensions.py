fa1bbc00a6cf3df7d47b6d7fd3f12e85e560ed37
Tapani Pälli <tapani.palli@intel.com>
2019-04-12 12:52:43 +0300

    anv: expose VK_EXT_queue_family_foreign on Android
    VK_ANDROID_external_memory_android_hardware_buffer requires this
    extension. It is safe to enable it since currently aux usage is
    disabled for ahw buffers.
    
    Fixes following dEQP extension dependency test on Android:
       dEQP-VK.api.info.device#extensions
    
    Cc: <mesa-stable@lists.freedesktop.org>
    Signed-off-by: Tapani Pälli <tapani.palli@intel.com>
    Acked-by: Jason Ekstrand <jason@jlekstrand.net>
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    (cherry picked from commit 376c3e8f87ab0b9bce5182da60c59832f7c1c138)

--- src/intel/vulkan/anv_extensions.py
+++ src/intel/vulkan/anv_extensions.py
@@ -128,6 +128,7 @@ EXTENSIONS = [
               'device->has_context_priority'),
     Extension('VK_EXT_pci_bus_info',                      2, True),
     Extension('VK_EXT_post_depth_coverage',               1, 'device->info.gen >= 9'),
+    Extension('VK_EXT_queue_family_foreign',              1, 'ANDROID'),
     Extension('VK_EXT_sampler_filter_minmax',             1, 'device->info.gen >= 9'),
     Extension('VK_EXT_scalar_block_layout',               1, True),
     Extension('VK_EXT_shader_viewport_index_layer',       1, True),
