65e30724295c12cab29eb2f0123ca61e39cf4b2e
Andres Gomez <agomez@igalia.com>
2019-04-20 01:47:09 +0200

    Revert "intel/fs: Add a scale factor to emit_fsign"
    This reverts commit 06d2c116415c0ab163a57ed7f2522342ed43e4d4.

--- src/intel/compiler/brw_fs.h
+++ src/intel/compiler/brw_fs.h
@@ -189,7 +189,7 @@ public:
    fs_reg resolve_source_modifiers(const fs_reg &src);
    void emit_discard_jump();
    void emit_fsign(const class brw::fs_builder &, const nir_alu_instr *instr,
-                   fs_reg result, fs_reg *op, unsigned fsign_src);
+                   fs_reg result, fs_reg *op);
    void emit_shader_float_controls_execution_mode();
    bool opt_peephole_sel();
    bool opt_peephole_csel();
