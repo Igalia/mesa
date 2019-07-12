9a3d618c738ed644e9efed466eb7f0d0b6cd3695
Rhys Perry <pendingchaos02@gmail.com>
2019-07-12 21:48:36 +0100

    amd/compiler: fix RADV_DEBUG=compiletime

--- src/amd/vulkan/radv_shader.c
+++ src/amd/vulkan/radv_shader.c
@@ -1107,7 +1107,7 @@ shader_variant_compile(struct radv_device *device,
 
 				clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &user1);
 				radv_compile_nir_shader(&ac_llvm, &binary, info,
-							shaders, shader_count, options);
+							llvm_shaders, shader_count, options);
 				clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &user2);
 
 				for (int i = 0; i < shader_count; i++)
